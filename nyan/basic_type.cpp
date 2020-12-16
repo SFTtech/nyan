f1a190e83dbc9e0f8cede2b783d87e7a8a2b7729
heinezen <heinezen@hotmail.de>
2020-12-16 01:22:11 +0100

    refactor: Add comments to basic_type.h.

--- nyan/basic_type.cpp
+++ nyan/basic_type.cpp
@@ -11,129 +11,129 @@
 namespace nyan {
 
 bool BasicType::is_object() const {
-	return (this->primitive_type == primitive_t::OBJECT);
+    return (this->primitive_type == primitive_t::OBJECT);
 }
 
 bool BasicType::is_fundamental() const {
-	switch (this->primitive_type) {
-	case primitive_t::BOOLEAN:
-	case primitive_t::TEXT:
-	case primitive_t::FILENAME:
-	case primitive_t::INT:
-	case primitive_t::FLOAT:
-	case primitive_t::NONE:
-		return true;
-	case primitive_t::OBJECT:
-	case primitive_t::CONTAINER:
-	case primitive_t::MODIFIER:
-		return false;
-	}
+    switch (this->primitive_type) {
+    case primitive_t::BOOLEAN:
+    case primitive_t::TEXT:
+    case primitive_t::FILENAME:
+    case primitive_t::INT:
+    case primitive_t::FLOAT:
+    case primitive_t::NONE:
+        return true;
+    case primitive_t::OBJECT:
+    case primitive_t::CONTAINER:
+    case primitive_t::MODIFIER:
+        return false;
+    }
 
-	throw InternalError{"unknown primitive type"};
+    throw InternalError{"unknown primitive type"};
 }
 
 
 bool BasicType::is_composite() const {
-	return (this->composite_type != composite_t::SINGLE);
+    return (this->composite_type != composite_t::SINGLE);
 }
 
 
 bool BasicType::is_container() const {
-	switch (this->composite_type) {
-	case composite_t::SET:
-	case composite_t::ORDEREDSET:
-	case composite_t::DICT:
-		return true;
-	case composite_t::SINGLE:
-	case composite_t::ABSTRACT:
-	case composite_t::CHILDREN:
-	case composite_t::OPTIONAL:
-		return false;
-	}
+    switch (this->composite_type) {
+    case composite_t::SET:
+    case composite_t::ORDEREDSET:
+    case composite_t::DICT:
+        return true;
+    case composite_t::SINGLE:
+    case composite_t::ABSTRACT:
+    case composite_t::CHILDREN:
+    case composite_t::OPTIONAL:
+        return false;
+    }
 
-	throw InternalError{"unknown composite type"};
+    throw InternalError{"unknown composite type"};
 }
 
 
 bool BasicType::is_modifier() const {
-	switch (this->composite_type) {
-	case composite_t::ABSTRACT:
-	case composite_t::CHILDREN:
-	case composite_t::OPTIONAL:
-		return true;
-	case composite_t::SINGLE:
-	case composite_t::SET:
-	case composite_t::ORDEREDSET:
-	case composite_t::DICT:
-		return false;
-	}
+    switch (this->composite_type) {
+    case composite_t::ABSTRACT:
+    case composite_t::CHILDREN:
+    case composite_t::OPTIONAL:
+        return true;
+    case composite_t::SINGLE:
+    case composite_t::SET:
+    case composite_t::ORDEREDSET:
+    case composite_t::DICT:
+        return false;
+    }
 
-	throw InternalError{"unknown composite type"};
+    throw InternalError{"unknown composite type"};
 }
 
 
 bool BasicType::operator ==(const BasicType &other) const {
-	return (this->primitive_type == other.primitive_type and
-	        this->composite_type == other.composite_type);
+    return (this->primitive_type == other.primitive_type and
+            this->composite_type == other.composite_type);
 }
 
 
 // textual type conversion for the type definition in a member
 BasicType BasicType::from_type_token(const IDToken &tok) {
-	// primitive type name map
-	static const std::unordered_map<std::string, primitive_t> primitive_types = {
-		{"bool", primitive_t::BOOLEAN},
-		{"text", primitive_t::TEXT},
-		{"file", primitive_t::FILENAME},
-		{"int", primitive_t::INT},
-		{"float", primitive_t::FLOAT}
-	};
+    // primitive type name map
+    static const std::unordered_map<std::string, primitive_t> primitive_types = {
+        {"bool", primitive_t::BOOLEAN},
+        {"text", primitive_t::TEXT},
+        {"file", primitive_t::FILENAME},
+        {"int", primitive_t::INT},
+        {"float", primitive_t::FLOAT}
+    };
 
-	// container type name map
-	static const std::unordered_map<std::string, composite_t> container_types = {
-		{"set", composite_t::SET},
-		{"orderedset", composite_t::ORDEREDSET},
-		{"dict", composite_t::DICT}
-	};
+    // container type name map
+    static const std::unordered_map<std::string, composite_t> container_types = {
+        {"set", composite_t::SET},
+        {"orderedset", composite_t::ORDEREDSET},
+        {"dict", composite_t::DICT}
+    };
 
-	// modifier type name map
-	static const std::unordered_map<std::string, composite_t> modifier_types = {
-		{"abstract", composite_t::ABSTRACT},
-		{"children", composite_t::CHILDREN},
-		{"optional", composite_t::OPTIONAL}
-	};
+    // modifier type name map
+    static const std::unordered_map<std::string, composite_t> modifier_types = {
+        {"abstract", composite_t::ABSTRACT},
+        {"children", composite_t::CHILDREN},
+        {"optional", composite_t::OPTIONAL}
+    };
 
-	primitive_t type = primitive_t::OBJECT;
-	composite_t composite_type = composite_t::SINGLE;
+    primitive_t type = primitive_t::OBJECT;
+    composite_t composite_type = composite_t::SINGLE;
 
-	switch (tok.get_type()) {
-		// type names are always identifiers:
-	case token_type::ID: {
-		auto it0 = primitive_types.find(tok.get_first());
-		if (it0 != std::end(primitive_types)) {
-			type = it0->second;
-			break;
-		}
+    switch (tok.get_type()) {
+        // type names are always identifiers:
+    case token_type::ID: {
+        auto it0 = primitive_types.find(tok.get_first());
+        if (it0 != std::end(primitive_types)) {
+            type = it0->second;
+            break;
+        }
 
-		auto it1 = container_types.find(tok.get_first());
-		if (it1 != std::end(container_types)) {
-			type = primitive_t::CONTAINER;
-			composite_type = it1->second;
-			break;
-		}
+        auto it1 = container_types.find(tok.get_first());
+        if (it1 != std::end(container_types)) {
+            type = primitive_t::CONTAINER;
+            composite_type = it1->second;
+            break;
+        }
 
-		auto it2 = modifier_types.find(tok.get_first());
-		if (it2 != std::end(modifier_types)) {
-			type = primitive_t::MODIFIER;
-			composite_type = it2->second;
-		}
-		break;
-	}
-	default:
-		throw ASTError{"expected some type name but there is", tok};
-	}
+        auto it2 = modifier_types.find(tok.get_first());
+        if (it2 != std::end(modifier_types)) {
+            type = primitive_t::MODIFIER;
+            composite_type = it2->second;
+        }
+        break;
+    }
+    default:
+        throw ASTError{"expected some type name but there is", tok};
+    }
 
-	return BasicType{type, composite_type};
+    return BasicType{type, composite_type};
 }
 
 } // namespace nyan
--- nyan/basic_type.h
+++ nyan/basic_type.h
@@ -17,15 +17,15 @@ class IDToken;
  * A MODIFIER is an extension of a contained type.
  */
 enum class primitive_t {
-	BOOLEAN,
-	TEXT,
-	FILENAME,
-	INT,
-	FLOAT,
-	OBJECT,
-	NONE,
-	CONTAINER,
-	MODIFIER,
+    BOOLEAN,
+    TEXT,
+    FILENAME,
+    INT,
+    FLOAT,
+    OBJECT,
+    NONE,
+    CONTAINER,
+    MODIFIER,
 };
 
 
@@ -34,18 +34,18 @@ enum class primitive_t {
  * SINGLE means it's not a composite.
  */
 enum class composite_t {
-	// primitive value
-	SINGLE,
+    // primitive value
+    SINGLE,
 
-	// Containers
-	SET,
-	ORDEREDSET,
-	DICT,
+    // Containers
+    SET,
+    ORDEREDSET,
+    DICT,
 
-	// Modifiers
-	ABSTRACT,
-	CHILDREN,
-	OPTIONAL,
+    // Modifiers
+    ABSTRACT,
+    CHILDREN,
+    OPTIONAL,
 };
 
 
@@ -59,141 +59,163 @@ enum class composite_t {
  */
 class BasicType {
 public:
-	/**
-	 * Primitive type.
-	 * Decides if this Type is primitive, an object, a container
-	 * or a modifier.
-	 */
-	primitive_t primitive_type;
+    /**
+     * Primitive type.
+     * Decides if this Type is primitive, an object, a container
+     * or a modifier.
+     */
+    primitive_t primitive_type;
 
-	/**
-	 * Stores if this type is a composite if types and if yes, which one.
-	 * Composite types can be containers or modifiers.
-	 */
-	composite_t composite_type;
+    /**
+     * Stores if this type is a composite if types and if yes, which one.
+     * Composite types can be containers or modifiers.
+     */
+    composite_t composite_type;
 
-	/**
-	 * Return whether the type is object.
-	 */
-	bool is_object() const;
-
-
-	/**
-	 * Return whether the given type is fundamental.
-	 * Primitive types are int, float, text, etc.
-	 * Non-primitive types are objects, containers and modifiers.
-	 */
-	bool is_fundamental() const;
+    /**
+     * Return whether the type is object.
+     *
+     * @return true if the basic type is an object, else false.
+     */
+    bool is_object() const;
 
 
-	/**
-	 * Test if this basic type is a composite.
-	 * that is, the composite type is not SINGLE.
-	 */
-	bool is_composite() const;
+    /**
+     * Return whether the given type is fundamental.
+     * Primitive types are int, float, text, etc.
+     * Non-primitive types are objects, containers and modifiers.
+     *
+     * @return true if the basic type is fundamental, else false.
+     */
+    bool is_fundamental() const;
 
 
-	/**
-	 * Test if this basic type is a container.
-	 * that is, the composite type is one of the container
-	 * types SET, ORDEREDSET or DICT.
-	 */
-	bool is_container() const;
+    /**
+     * Test if this basic type is a composite.
+     * that is, the composite type is not SINGLE.
+     *
+     * @return true if the basic type is a composite, else false.
+     */
+    bool is_composite() const;
 
 
-	/**
-	 * Test if this basic type is a modifier.
-	 * that is, the composite type is one of the modifier
-	 * types ABSTRACT, CHILDREN or OPTIONAL.
-	 */
-	bool is_modifier() const;
+    /**
+     * Test if this basic type is a container.
+     * that is, the composite type is one of the container
+     * types SET, ORDEREDSET or DICT.
+     *
+     * @return true if the basic type is a container, else false.
+     */
+    bool is_container() const;
 
 
-	/**
-	 * Equality comparison.
-	 */
-	bool operator ==(const BasicType &other) const;
+    /**
+     * Test if this basic type is a modifier.
+     * that is, the composite type is one of the modifier
+     * types ABSTRACT, CHILDREN or OPTIONAL.
+     *
+     * @return true if the basic type is a modifier, else false.
+     */
+    bool is_modifier() const;
 
 
-	/**
-	 * Test if the given type token declares a valid primitive
-	 * and composite type.
-	 * A type token is e.g. "int" or "float" or "SomeObject".
-	 * If it is e.g. "set", type will be CONTAINER and the composite type SET.
-	 *
-	 * @param token An IDToken that contains a type identifier or a nyan object name.
-	 * @return BasicType declared by the token.
-	 * @throw ASTError if it fails.
-	 */
-	static BasicType from_type_token(const IDToken &token);
+    /**
+     * Equality comparison.
+     *
+     * @return true if the basic types are exactly the same, else false.
+     */
+    bool operator ==(const BasicType &other) const;
 
 
-	/**
-	 * Returns how many nested types are required when declaring a given
-	 * type.
-	 *
-	 * @param type The tested type.
-	 * @return Number of required nested types.
-	 */
-	static unsigned int expected_nested_types(const BasicType &type) {
-		if (type.is_fundamental()) {
-			return 0;
-		}
+    /**
+     * Test if the given type token declares a valid primitive
+     * and composite type.
+     * A type token is e.g. "int" or "float" or "SomeObject".
+     * If it is e.g. "set", type will be CONTAINER and the composite type SET.
+     *
+     * @param token An IDToken that contains a type identifier or a nyan object name.
+     *
+     * @return BasicType declared by the token.
+     * @throw ASTError if no typename could be found.
+     */
+    static BasicType from_type_token(const IDToken &token);
 
-		switch (type.composite_type) {
-		// containers
-		case composite_t::SET:        return 1;
-		case composite_t::ORDEREDSET: return 1;
-		case composite_t::DICT:       return 2;
 
-		// modifiers
-		case composite_t::ABSTRACT:   return 1;
-		case composite_t::CHILDREN:   return 1;
-		case composite_t::OPTIONAL:   return 1;
+    /**
+     * Returns how many nested types are required when declaring a given
+     * type.
+     *
+     * @param type The tested type.
+     *
+     * @return Number of required nested types.
+     */
+    static unsigned int expected_nested_types(const BasicType &type) {
+        if (type.is_fundamental()) {
+            return 0;
+        }
 
-		// else, must be an object
-		default:
-			return 0;
-		}
-	}
+        switch (type.composite_type) {
+        // containers
+        case composite_t::SET:        return 1;
+        case composite_t::ORDEREDSET: return 1;
+        case composite_t::DICT:       return 2;
+
+        // modifiers
+        case composite_t::ABSTRACT:   return 1;
+        case composite_t::CHILDREN:   return 1;
+        case composite_t::OPTIONAL:   return 1;
+
+        // else, must be an object
+        default:
+            return 0;
+        }
+    }
 };
 
 
 /**
  * Get a string representation of a basic nyan type.
+ *
+ * @param type A primitive type.
+ *
+ * @return String representation of the type.
  */
 constexpr const char *type_to_string(primitive_t type) {
-	switch (type) {
-	case primitive_t::BOOLEAN:       return "bool";
-	case primitive_t::TEXT:          return "text";
-	case primitive_t::FILENAME:      return "file";
-	case primitive_t::INT:           return "int";
-	case primitive_t::FLOAT:         return "float";
-	case primitive_t::OBJECT:        return "object";
-	case primitive_t::NONE:          return "none";
-	case primitive_t::CONTAINER:     return "container";
-	case primitive_t::MODIFIER:      return "modifier";
-	}
+    switch (type) {
+    case primitive_t::BOOLEAN:       return "bool";
+    case primitive_t::TEXT:          return "text";
+    case primitive_t::FILENAME:      return "file";
+    case primitive_t::INT:           return "int";
+    case primitive_t::FLOAT:         return "float";
+    case primitive_t::OBJECT:        return "object";
+    case primitive_t::NONE:          return "none";
+    case primitive_t::CONTAINER:     return "container";
+    case primitive_t::MODIFIER:      return "modifier";
+    }
 
-	return "unhandled primitive_t";
+    return "unhandled primitive_t";
 }
 
 
 /**
  * Get a string represenation for a nyan composite type.
+ *
+ * @param type A composite type.
+ *
+ * @return String representation of the type.
  */
 constexpr const char *composite_type_to_string(composite_t type) {
-	switch (type) {
-	case composite_t::SINGLE:        return "single_value";
-	case composite_t::SET:           return "set";
-	case composite_t::ORDEREDSET:    return "orderedset";
-	case composite_t::DICT:          return "dict";
-	case composite_t::ABSTRACT:      return "abstract";
-	case composite_t::CHILDREN:      return "children";
-	case composite_t::OPTIONAL:      return "optional";
-	}
+    switch (type) {
+    case composite_t::SINGLE:        return "single_value";
+    case composite_t::SET:           return "set";
+    case composite_t::ORDEREDSET:    return "orderedset";
+    case composite_t::DICT:          return "dict";
+    case composite_t::ABSTRACT:      return "abstract";
+    case composite_t::CHILDREN:      return "children";
+    case composite_t::OPTIONAL:      return "optional";
+    }
 
-	return "unhandled composite_t";
+    return "unhandled composite_t";
 }
 
 } // namespace nyan
