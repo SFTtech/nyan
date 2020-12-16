9c2a7fe8db038da6303d81c1958b8f3d71cbaaed
heinezen <heinezen@hotmail.de>
2020-12-16 03:38:32 +0100

    refactor: Add comments to error.h.

--- nyan/error.cpp
+++ nyan/error.cpp
@@ -24,59 +24,59 @@ using namespace std::string_literals;
 
 
 void Backtrace::analyze() {
-	std::vector<void *> buffer{32};
+    std::vector<void *> buffer{32};
 
-	// increase buffer size until it's enough
-	while (true) {
-		int elements = backtrace(buffer.data(), buffer.size());
-		if (elements < static_cast<decltype(elements)>(buffer.size())) {
-			buffer.resize(elements);
-			break;
-		}
-		buffer.resize(buffer.size() * 2);
-	}
+    // increase buffer size until it's enough
+    while (true) {
+        int elements = backtrace(buffer.data(), buffer.size());
+        if (elements < static_cast<decltype(elements)>(buffer.size())) {
+            buffer.resize(elements);
+            break;
+        }
+        buffer.resize(buffer.size() * 2);
+    }
 
-	for (void *element : buffer) {
-		this->stack_addrs.push_back(element);
-	}
+    for (void *element : buffer) {
+        this->stack_addrs.push_back(element);
+    }
 }
 
 
 void Backtrace::get_symbols(std::function<void (const backtrace_symbol *)> cb, bool reversed) const {
-	backtrace_symbol symbol;
+    backtrace_symbol symbol;
 
-	if (reversed) {
-		for (size_t idx = this->stack_addrs.size(); idx-- > 0;) {
-			void *pc = this->stack_addrs[idx];
+    if (reversed) {
+        for (size_t idx = this->stack_addrs.size(); idx-- > 0;) {
+            void *pc = this->stack_addrs[idx];
 
-			symbol.functionname = util::symbol_name(pc, false, true);
-			symbol.pc = pc;
+            symbol.functionname = util::symbol_name(pc, false, true);
+            symbol.pc = pc;
 
-			cb(&symbol);
-		}
-	} else {
-		for (void *pc : this->stack_addrs) {
-			symbol.functionname = util::symbol_name(pc, false, true);
-			symbol.pc = pc;
+            cb(&symbol);
+        }
+    } else {
+        for (void *pc : this->stack_addrs) {
+            symbol.functionname = util::symbol_name(pc, false, true);
+            symbol.pc = pc;
 
-			cb(&symbol);
-		}
-	}
+            cb(&symbol);
+        }
+    }
 }
 
 
 void Backtrace::trim_to_current_stack_frame() {
-	Backtrace current;
-	current.analyze();
+    Backtrace current;
+    current.analyze();
 
-	while (not current.stack_addrs.empty() and not this->stack_addrs.empty()) {
-		if (this->stack_addrs.back() != current.stack_addrs.back()) {
-			break;
-		}
+    while (not current.stack_addrs.empty() and not this->stack_addrs.empty()) {
+        if (this->stack_addrs.back() != current.stack_addrs.back()) {
+            break;
+        }
 
-		this->stack_addrs.pop_back();
-		current.stack_addrs.pop_back();
-	}
+        this->stack_addrs.pop_back();
+        current.stack_addrs.pop_back();
+    }
 }
 
 
@@ -88,117 +88,117 @@ bool Error::break_on_error = false;
 Error::Error(const std::string &msg,
              bool generate_backtrace,
              bool store_cause)
-	:
-	std::runtime_error{runtime_error_message},
-	backtrace{nullptr},
-	msg{msg} {
+    :
+    std::runtime_error{runtime_error_message},
+    backtrace{nullptr},
+    msg{msg} {
 
-	if (generate_backtrace) {
-		this->backtrace = std::make_shared<Backtrace>();
-		this->backtrace->analyze();
-	}
+    if (generate_backtrace) {
+        this->backtrace = std::make_shared<Backtrace>();
+        this->backtrace->analyze();
+    }
 
-	if (store_cause) {
-		this->store_cause();
-	}
+    if (store_cause) {
+        this->store_cause();
+    }
 
-	if (unlikely(this->break_on_error)) {
-		BREAKPOINT;
-	}
+    if (unlikely(this->break_on_error)) {
+        BREAKPOINT;
+    }
 }
 
 
 std::string Error::str() const {
-	return this->msg;
+    return this->msg;
 }
 
 
 const char *Error::what() const noexcept {
-	this->what_cache = this->str();
-	return this->what_cache.c_str();
+    this->what_cache = this->str();
+    return this->what_cache.c_str();
 }
 
 
 void Error::store_cause() {
-	if (not std::current_exception()) {
-		return;
-	}
+    if (not std::current_exception()) {
+        return;
+    }
 
-	try {
-		throw;
-	} catch (Error &cause) {
-		cause.trim_backtrace();
-		this->cause = std::current_exception();
-	} catch (...) {
-		this->cause = std::current_exception();
-	}
+    try {
+        throw;
+    } catch (Error &cause) {
+        cause.trim_backtrace();
+        this->cause = std::current_exception();
+    } catch (...) {
+        this->cause = std::current_exception();
+    }
 }
 
 
 void Error::trim_backtrace() {
-	if (this->backtrace) {
-		this->backtrace->trim_to_current_stack_frame();
-	}
+    if (this->backtrace) {
+        this->backtrace->trim_to_current_stack_frame();
+    }
 }
 
 
 void Error::rethrow_cause() const {
-	if (this->cause) {
-		std::rethrow_exception(this->cause);
-	}
+    if (this->cause) {
+        std::rethrow_exception(this->cause);
+    }
 }
 
 
 std::string Error::type_name() const {
-	return util::demangle(typeid(*this).name());
+    return util::demangle(typeid(*this).name());
 }
 
 
 Backtrace *Error::get_backtrace() const {
-	return this->backtrace.get();
+    return this->backtrace.get();
 }
 
 
 const std::string &Error::get_msg() const {
-	return this->msg;
+    return this->msg;
 }
 
 
 void Error::enable_break(bool enable) {
-	Error::break_on_error = enable;
+    Error::break_on_error = enable;
 }
 
 
 std::ostream &operator <<(std::ostream &os, const Error &e) {
-	// output the exception cause
-	bool had_a_cause = true;
-	try {
-		e.rethrow_cause();
-		had_a_cause = false;
-	} catch (Error &cause) {
-		os << cause << std::endl;
-	} catch (std::exception &cause) {
-		os << util::demangle(typeid(cause).name()) << ": " << cause.what() << std::endl;
-	}
+    // output the exception cause
+    bool had_a_cause = true;
+    try {
+        e.rethrow_cause();
+        had_a_cause = false;
+    } catch (Error &cause) {
+        os << cause << std::endl;
+    } catch (std::exception &cause) {
+        os << util::demangle(typeid(cause).name()) << ": " << cause.what() << std::endl;
+    }
 
-	if (had_a_cause) {
-		os << std::endl
-		   << "The above exception was the direct cause "
-		      "of the following exception:"
-		   << std::endl << std::endl;
-	}
+    if (had_a_cause) {
+        os << std::endl
+           << "The above exception was the direct cause "
+              "of the following exception:"
+           << std::endl << std::endl;
+    }
 
-	// output the exception backtrace
-	if (e.get_backtrace()) {
-		os << *e.get_backtrace();
-	} else {
-		os << "origin:" << std::endl;
-	}
+    // output the exception backtrace
+    if (e.get_backtrace()) {
+        os << *e.get_backtrace();
+    } else {
+        os << "origin:" << std::endl;
+    }
 
-	os << e.type_name() << ":" << std::endl;
-	os << e.str();
+    os << e.type_name() << ":" << std::endl;
+    os << e.str();
 
-	return os;
+    return os;
 }
 
 
@@ -206,20 +206,20 @@ std::ostream &operator <<(std::ostream &os, const Error &e) {
  * Prints a backtrace_symbol object.
  */
 std::ostream &operator <<(std::ostream &os, const backtrace_symbol &bt_sym) {
-	// imitate the looks of a Python traceback.
-	os << " -> ";
+    // imitate the looks of a Python traceback.
+    os << " -> ";
 
-	if (bt_sym.functionname.empty()) {
-		os << '?';
-	} else {
-		os << bt_sym.functionname;
-	}
+    if (bt_sym.functionname.empty()) {
+        os << '?';
+    } else {
+        os << bt_sym.functionname;
+    }
 
-	if (bt_sym.pc != nullptr) {
-		os << " [" << bt_sym.pc << "]";
-	}
+    if (bt_sym.pc != nullptr) {
+        os << " [" << bt_sym.pc << "]";
+    }
 
-	return os;
+    return os;
 }
 
 
@@ -227,24 +227,24 @@ std::ostream &operator <<(std::ostream &os, const backtrace_symbol &bt_sym) {
  * Prints an entire Backtrace object.
  */
 std::ostream &operator <<(std::ostream &os, const Backtrace &bt) {
-	// imitate the looks of a Python traceback.
-	os << "Traceback (most recent call last):" << std::endl;
+    // imitate the looks of a Python traceback.
+    os << "Traceback (most recent call last):" << std::endl;
 
-	bt.get_symbols([&os](const backtrace_symbol *symbol) {
-		os << *symbol << std::endl;
-	}, true);
+    bt.get_symbols([&os](const backtrace_symbol *symbol) {
+        os << *symbol << std::endl;
+    }, true);
 
-	return os;
+    return os;
 }
 
 
 InternalError::InternalError(const std::string &msg)
-	:
-	Error{msg} {}
+    :
+    Error{msg} {}
 
 
 FileReadError::FileReadError(const std::string &msg)
-	:
-	Error{msg} {}
+    :
+    Error{msg} {}
 
 } // namespace nyan
--- nyan/error.h
+++ nyan/error.h
@@ -19,8 +19,8 @@ namespace nyan {
  * Backtrace::get_symbols().
  */
 struct backtrace_symbol {
-	std::string functionname;  // empty if unknown
-	void *pc;                  // nullptr if unknown
+    std::string functionname;  // empty if unknown
+    void *pc;                  // nullptr if unknown
 };
 
 
@@ -29,45 +29,45 @@ struct backtrace_symbol {
  */
 class Backtrace {
 public:
-	Backtrace() = default;
+    Backtrace() = default;
 
-	virtual ~Backtrace() = default;
+    virtual ~Backtrace() = default;
 
-	/**
-	 * Analyzes the current stack, and stores the program counter values in
-	 * this->stack_addrs.
-	 */
-	void analyze();
+    /**
+     * Analyzes the current stack, and stores the program counter values in
+     * this->stack_addrs.
+     */
+    void analyze();
 
-	/**
-	 * Provide the names for all stack frames via the callback.
-	 *
-	 * The most recent call is returned last (alike Python).
-	 *
-	 * @param cb
-	 *    is called for every symbol in the backtrace,
-	 *    starting with the top-most frame.
-	 * @param reversed
-	 *    if true, the most recent call is given last.
-	 */
-	void get_symbols(std::function<void (const backtrace_symbol *)> cb,
-	                 bool reversed=true) const;
+    /**
+     * Provide the names for all stack frames via the callback.
+     *
+     * The most recent call is returned last (alike Python).
+     *
+     * @param cb
+     *    is called for every symbol in the backtrace,
+     *    starting with the top-most frame.
+     * @param reversed
+     *	  if true, the most recent call is given last.
+     */
+    void get_symbols(std::function<void (const backtrace_symbol *)> cb,
+                     bool reversed=true) const;
 
-	/**
-	 * Removes all the lower frames that are also present
-	 * in the current stack.
-	 *
-	 * Designed to be used in catch clauses,
-	 * to simulate stack trace collection
-	 * from throw to catch, instead of from throw to the process entry point.
-	 */
-	void trim_to_current_stack_frame();
+    /**
+     * Removes all the lower frames that are also present
+     * in the current stack.
+     *
+     * Designed to be used in catch clauses,
+     * to simulate stack trace collection
+     * from throw to catch, instead of from throw to the process entry point.
+     */
+    void trim_to_current_stack_frame();
 
 protected:
-	/**
-	 * All program counters of this backtrace.
-	 */
-	std::vector<void *> stack_addrs;
+    /**
+     * All program counters of this backtrace.
+     */
+    std::vector<void *> stack_addrs;
 };
 
 
@@ -76,110 +76,131 @@ protected:
  */
 class Error : public std::runtime_error {
 public:
-	Error(const std::string &msg,
-	      bool generate_backtrace=true,
-	      bool store_cause=true);
+    Error(const std::string &msg,
+          bool generate_backtrace=true,
+          bool store_cause=true);
 
-	virtual ~Error() = default;
+    virtual ~Error() = default;
 
-	/**
-	 * String representation of this exception, as
-	 * specialized by a child exception.
-	 */
-	virtual std::string str() const;
+    /**
+     * String representation of this exception, as
+     * specialized by a child exception.
+     *
+     * @return String represantation of this exception.
+     */
+    virtual std::string str() const;
 
-	/**
-	 * Returns the message's content.
-	 */
-	const char *what() const noexcept override;
+    /**
+     * Returns the message's content.
+     *
+     * @return Char array containing the error message.
+     */
+    const char *what() const noexcept override;
 
-	/**
-	 * Stores a pointer to the currently-handled exception in this->cause.
-	 */
-	void store_cause();
+    /**
+     * Stores a pointer to the currently-handled exception in this->cause.
+     */
+    void store_cause();
 
-	/**
-	 * Calls this->backtrace->trim_to_current_stack_frame(),
-	 * if this->backtrace is not nullptr.
-	 *
-	 * Designed to be used in catch clauses, to strip away all those
-	 * unneeded symbols from program init upwards.
-	 */
-	void trim_backtrace();
+    /**
+     * Calls this->backtrace->trim_to_current_stack_frame(),
+     * if this->backtrace is not nullptr.
+     *
+     * Designed to be used in catch clauses, to strip away all those
+     * unneeded symbols from program init upwards.
+     */
+    void trim_backtrace();
 
-	/**
-	 * Re-throws the exception cause, if the exception has one.
-	 * Otherwise, does nothing.
-	 *
-	 * Use this when handling the exception, to handle the cause.
-	 */
-	void rethrow_cause() const;
+    /**
+     * Re-throws the exception cause, if the exception has one.
+     * Otherwise, does nothing.
+     *
+     * Use this when handling the exception, to handle the cause.
+     */
+    void rethrow_cause() const;
 
-	/**
-	 * Get the type name of of the exception.
-	 * Use it to display the name of a child exception.
-	 */
-	virtual std::string type_name() const;
+    /**
+     * Get the type name of of the exception.
+     * Use it to display the name of a child exception.
+     *
+     * @return String with the exception type.
+     */
+    virtual std::string type_name() const;
 
-	/**
-	 * Return the backtrace where the exception was thrown.
-	 * nullptr if no backtrace was collected.
-	 */
-	Backtrace *get_backtrace() const;
+    /**
+     * Return the backtrace where the exception was thrown.
+     * nullptr if no backtrace was collected.
+     *
+     * @return Backtrace to the exception's origin.
+     */
+    Backtrace *get_backtrace() const;
 
-	/**
-	 * Directly return the message stored in the exception.
-	 */
-	const std::string &get_msg() const;
+    /**
+     * Directly return the message stored in the exception.
+     *
+     * @return String containing the error message.
+     */
+    const std::string &get_msg() const;
 
-	/**
-	 * Enable invocation of software breakpoint
-	 * when this Error is constructed.
-	 */
-	static void enable_break(bool enable);
+    /**
+     * Enable invocation of software breakpoint
+     * when this Error is constructed.
+     *
+     * @param enable If true, enable a breakpoint, else disable it.
+     */
+    static void enable_break(bool enable);
 
 protected:
-	/**
-	 * The (optional) backtrace where the exception came from.
-	 */
-	std::shared_ptr<Backtrace> backtrace;
+    /**
+     * The (optional) backtrace where the exception came from.
+     */
+    std::shared_ptr<Backtrace> backtrace;
 
-	/**
-	 * The error message text.
-	 */
-	std::string msg;
+    /**
+     * The error message text.
+     */
+    std::string msg;
 
-	/**
-	 * Cached error message text for returning C string via what().
-	 */
-	mutable std::string what_cache;
+    /**
+     * Cached error message text for returning C string via what().
+     */
+    mutable std::string what_cache;
 
-	/**
-	 * Re-throw this with rethrow_cause().
-	 */
-	std::exception_ptr cause;
+    /**
+     * Re-throw this with rethrow_cause().
+     */
+    std::exception_ptr cause;
 
-	/**
-	 * Issue software breakpoint when an error is constructed.
-	 */
-	static bool break_on_error;
+    /**
+     * Issue software breakpoint when an error is constructed.
+     */
+    static bool break_on_error;
 };
 
 
 /**
  * Output stream concat for nyanerrors.
+ *
+ * @param os Output stream the error is appended to.
+ * @param e Error whose message is appended to the output stream.
  */
 std::ostream &operator <<(std::ostream &os, const Error &e);
 
 
 /**
  * Output stream concat for backtrace symbols.
+ *
+ * @param os Output stream the backtrace symbol is appended to.
+ * @param bt_sym Backtrace symbol which is appended to the output stream.
  */
 std::ostream &operator <<(std::ostream &os, const backtrace_symbol &bt_sym);
 
 
 /**
  * Output stream concat for backtraces.
+ *
+ * @param os Output stream the backtrace is appended to.
+ * @param bt Backtrace which is appended to the output stream.
  */
 std::ostream &operator <<(std::ostream &os, const Backtrace &bt);
 
@@ -189,7 +210,7 @@ std::ostream &operator <<(std::ostream &os, const Backtrace &bt);
  */
 class InternalError : public Error {
 public:
-	InternalError(const std::string &msg);
+    InternalError(const std::string &msg);
 };
 
 
@@ -200,7 +221,7 @@ public:
  */
 class FileReadError : public Error {
 public:
-	FileReadError(const std::string &msg);
+    FileReadError(const std::string &msg);
 };
 
 } // namespace nyan
