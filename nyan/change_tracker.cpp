78cd996eaff9fbe8bf97030cddc2652c6c2c488c
heinezen <heinezen@hotmail.de>
2020-12-16 01:42:36 +0100

    refactor: Add comments to change_tracker.h.

--- nyan/change_tracker.cpp
+++ nyan/change_tracker.cpp
@@ -6,50 +6,50 @@
 namespace nyan {
 
 void ObjectChanges::add_parent(const fqon_t &obj) {
-	this->new_parents.push_back(obj);
+    this->new_parents.push_back(obj);
 }
 
 
 const std::vector<fqon_t> &ObjectChanges::get_new_parents() const {
-	return this->new_parents;
+    return this->new_parents;
 }
 
 
 bool ObjectChanges::parents_update_required() const {
-	return this->new_parents.size() > 0;
+    return this->new_parents.size() > 0;
 }
 
 
 ObjectChanges &ChangeTracker::track_patch(const fqon_t &target_name) {
-	// if existing, return the object change tracker
-	// else: create a new one.
-	auto it = this->changes.find(target_name);
-	if (it == std::end(this->changes)) {
-		return this->changes.emplace(
-			target_name,
-			ObjectChanges{}
-		).first->second;
-	}
-	else {
-		return it->second;
-	}
+    // if existing, return the object change tracker
+    // else: create a new one.
+    auto it = this->changes.find(target_name);
+    if (it == std::end(this->changes)) {
+        return this->changes.emplace(
+            target_name,
+            ObjectChanges{}
+        ).first->second;
+    }
+    else {
+        return it->second;
+    }
 }
 
 
 const std::unordered_map<fqon_t, ObjectChanges> &ChangeTracker::get_object_changes() const {
-	return this->changes;
+    return this->changes;
 }
 
 
 std::unordered_set<fqon_t> ChangeTracker::get_changed_objects() const {
-	std::unordered_set<fqon_t> ret;
-	ret.reserve(this->changes.size());
+    std::unordered_set<fqon_t> ret;
+    ret.reserve(this->changes.size());
 
-	for (auto &it : this->changes) {
-		ret.insert(it.first);
-	}
+    for (auto &it : this->changes) {
+        ret.insert(it.first);
+    }
 
-	return ret;
+    return ret;
 }
 
 
--- nyan/change_tracker.h
+++ nyan/change_tracker.h
@@ -16,13 +16,32 @@ namespace nyan {
  */
 class ObjectChanges {
 public:
-	void add_parent(const fqon_t &obj);
+    /**
+     * Track an object as a new parent.
+     *
+     * @param obj Identifier of the object.
+     */
+    void add_parent(const fqon_t &obj);
 
-	const std::vector<fqon_t> &get_new_parents() const;
-	bool parents_update_required() const;
+    /**
+     * Retrieve the list of new parents.
+     *
+     * @return The list of new parents.
+     */
+    const std::vector<fqon_t> &get_new_parents() const;
+
+    /**
+     * Check if the parents were updated by the change.
+     *
+     * @return true if size of new_parents is > 1, else false.
+     */
+    bool parents_update_required() const;
 
 protected:
-	std::vector<fqon_t> new_parents;
+    /**
+     * List of new parents.
+     */
+    std::vector<fqon_t> new_parents;
 };
 
 
@@ -32,14 +51,37 @@ protected:
  */
 class ChangeTracker {
 public:
-	ObjectChanges &track_patch(const fqon_t &target_name);
 
-	const std::unordered_map<fqon_t, ObjectChanges> &get_object_changes() const;
+    /**
+     * Get the ObjectChanges for an object targeted by a patch
+     * from the changes map or create a new one if there doesn't
+     * exist one yet.
+     *
+     * @param target_name Identifier of the target object.
+     *
+     * @return An ObjectChanges tracker.
+     */
+    ObjectChanges &track_patch(const fqon_t &target_name);
 
-	std::unordered_set<fqon_t> get_changed_objects() const;
+    /**
+     * Retrieve the map with all ObjectChanges by object.
+     *
+     * @return A map of ObjectChanges by object.
+     */
+    const std::unordered_map<fqon_t, ObjectChanges> &get_object_changes() const;
+
+    /**
+     * Get all objects whose ObjectChanges are tracked in this tracker.
+     *
+     * @return A set of object identifiers.
+     */
+    std::unordered_set<fqon_t> get_changed_objects() const;
 
 protected:
-	std::unordered_map<fqon_t, ObjectChanges> changes;
+    /**
+     * Map of ObjectChanges by object.
+     */
+    std::unordered_map<fqon_t, ObjectChanges> changes;
 };
 
 } // namespace nyan
