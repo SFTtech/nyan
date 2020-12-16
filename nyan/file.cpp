06212ba80b457cb26a8e8ae28313a2a110c75500
heinezen <heinezen@hotmail.de>
2020-12-16 04:25:27 +0100

    refactor: Add comments to file.h.

--- nyan/file.cpp
+++ nyan/file.cpp
@@ -10,58 +10,58 @@ namespace nyan {
 
 
 File::File(const std::string &virtual_name, std::string &&data)
-	:
-	name{virtual_name},
-	data{std::move(data)} {
+    :
+    name{virtual_name},
+    data{std::move(data)} {
 
-	this->extract_lines();
+    this->extract_lines();
 }
 
 
 File::File(const std::string &path)
-	:
-	File{path, util::read_file(path)} {
+    :
+    File{path, util::read_file(path)} {
 
-	// util::read_file throws a FileReadError if unsuccessful.
+    // util::read_file throws a FileReadError if unsuccessful.
 }
 
 
 void File::extract_lines() {
-	this->line_ends = { std::string::npos };
+    this->line_ends = { std::string::npos };
 
-	for (size_t i = 0; i < this->data.size(); i++) {
-		if (this->data[i] == '\n') {
-			this->line_ends.push_back(i);
-		}
-	}
-	this->line_ends.push_back(data.size());
+    for (size_t i = 0; i < this->data.size(); i++) {
+        if (this->data[i] == '\n') {
+            this->line_ends.push_back(i);
+        }
+    }
+    this->line_ends.push_back(data.size());
 }
 
 
 const std::string &File::get_name() const {
-	return this->name;
+    return this->name;
 }
 
 
 const std::string &File::get_content() const {
-	return this->data;
+    return this->data;
 }
 
 
 std::string File::get_line(size_t n) const {
-	size_t begin = this->line_ends[n - 1] + 1;
-	size_t len = this->line_ends[n] - begin;
-	return this->data.substr(begin, len);
+    size_t begin = this->line_ends[n - 1] + 1;
+    size_t len = this->line_ends[n] - begin;
+    return this->data.substr(begin, len);
 }
 
 
 const char *File::c_str() const {
-	return this->data.c_str();
+    return this->data.c_str();
 }
 
 
 size_t File::size() const {
-	return this->data.size();
+    return this->data.size();
 }
 
 
--- nyan/file.h
+++ nyan/file.h
@@ -14,65 +14,86 @@ namespace nyan {
  */
 class File {
 public:
-	File(const std::string &path);
-	File(const std::string &virtual_name, std::string &&data);
+    File(const std::string &path);
+    File(const std::string &virtual_name, std::string &&data);
 
-	// moving allowed
-	File(File &&other) noexcept = default;
-	File& operator =(File &&other) noexcept = default;
+    // moving allowed
+    File(File &&other) noexcept = default;
+    File& operator =(File &&other) noexcept = default;
 
-	// no copies
-	File(const File &other) = delete;
-	File &operator =(const File &other) = delete;
+    // no copies
+    File(const File &other) = delete;
+    File &operator =(const File &other) = delete;
 
-	virtual ~File() = default;
+    virtual ~File() = default;
 
-	/**
-	 * Return the file name.
-	 */
-	const std::string &get_name() const;
+    /**
+     * Return the file name.
+     *
+     * @return String containing the filename.
+     */
+    const std::string &get_name() const;
 
-	/**
-	 * Return the file content.
-	 */
-	const std::string &get_content() const;
+    /**
+     * Return the file content.
+     *
+     * @return String containing the file's content.
+     */
+    const std::string &get_content() const;
 
-	/**
-	 * Return the given line number of the file.
-	 * Starts at line 1. *buhuuuuu* *sob* *mrrrmmuu* *whimper*
-	 */
-	std::string get_line(size_t n) const;
+    /**
+     * Return the given line number of the file.
+     * Starts at line 1. *buhuuuuu* *sob* *mrrrmmuu* *whimper*
+     *
+     * @param n Line number.
+     *
+     * @return String containing the content of line n.
+     */
+    std::string get_line(size_t n) const;
 
-	/**
-	 * Return the number of lines in the file.
-	 * It will always have at least one line.
-	 * The empty file has one line of length 0.
-	 */
-	size_t get_line_count() const;
+    /**
+     * Return the number of lines in the file.
+     * It will always have at least one line.
+     * The empty file has one line of length 0.
+     *
+     * @return Number of lines in the file.
+     */
+    size_t get_line_count() const;
 
-	/**
-	 * Return a c string of the file content.
-	 */
-	const char *c_str() const;
+    /**
+     * Return a c string of the file content.
+     *
+     * @return Char array containing the filename.
+     */
+    const char *c_str() const;
 
-	/**
-	 * Return the size of the file content.
-	 */
-	size_t size() const;
+    /**
+     * Return the size of the file content.
+     *
+     * @return Size of the file content string in bytes.
+     */
+    size_t size() const;
 
 protected:
-	/**
-	 * Create line_ends entries from the file content.
-	 */
-	void extract_lines();
+    /**
+     * Create line_ends entries from the file content.
+     */
+    void extract_lines();
 
-	std::string name;
-	std::string data;
+    /**
+     * Name of the file.
+     */
+    std::string name;
 
-	/**
-	 * Stores the offsets of line endings in the file content.
-	 */
-	std::vector<size_t> line_ends;
+    /**
+     * Content of the file.
+     */
+    std::string data;
+
+    /**
+     * Stores the offsets of line endings in the file content.
+     */
+    std::vector<size_t> line_ends;
 };
 
 } // namespace std
