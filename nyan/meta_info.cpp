312914b5bd4d9542927da47fbbd4170a3e393886
heinezen <heinezen@hotmail.de>
2020-12-20 00:47:50 +0100

    refactor: Add comments to meta_info.h.

--- nyan/meta_info.cpp
+++ nyan/meta_info.cpp
@@ -9,60 +9,60 @@
 
 namespace nyan {
 
-ObjectInfo &MetaInfo::add_object(const fqon_t &name, ObjectInfo &&obj) {
-	// copy location so we can use it after obj was moved.
-	Location loc = obj.get_location();
+ObjectInfo &MetaInfo::add_object(const fqon_t &name, ObjectInfo &&obj_info) {
+    // copy location so we can use it after obj was moved.
+    Location loc = obj_info.get_location();
 
-	auto ret = this->object_info.insert({name, std::move(obj)});
-	if (ret.second == false) {
-		throw LangError{
-			loc,
-			"object already defined",
-			{{ret.first->second.get_location(), "first defined here"}}
-		};
-	}
+    auto ret = this->object_info.insert({name, std::move(obj_info)});
+    if (ret.second == false) {
+        throw LangError{
+            loc,
+            "object already defined",
+            {{ret.first->second.get_location(), "first defined here"}}
+        };
+    }
 
-	return ret.first->second;
+    return ret.first->second;
 }
 
 
 const MetaInfo::obj_info_t &MetaInfo::get_objects() const {
-	return this->object_info;
+    return this->object_info;
 }
 
 
 ObjectInfo *MetaInfo::get_object(const fqon_t &name) {
-	auto it = this->object_info.find(name);
-	if (it == std::end(this->object_info)) {
-		return nullptr;
-	}
-	return &it->second;
+    auto it = this->object_info.find(name);
+    if (it == std::end(this->object_info)) {
+        return nullptr;
+    }
+    return &it->second;
 }
 
 
 // Thanks C++ for the beautiful duplication
 const ObjectInfo *MetaInfo::get_object(const fqon_t &name) const {
-	auto it = this->object_info.find(name);
-	if (it == std::end(this->object_info)) {
-		return nullptr;
-	}
-	return &it->second;
+    auto it = this->object_info.find(name);
+    if (it == std::end(this->object_info)) {
+        return nullptr;
+    }
+    return &it->second;
 }
 
 
 bool MetaInfo::has_object(const fqon_t &name) const {
-	return (this->object_info.find(name) != std::end(this->object_info));
+    return (this->object_info.find(name) != std::end(this->object_info));
 }
 
 
 std::string MetaInfo::str() const {
-	std::ostringstream builder;
+    std::ostringstream builder;
 
-	for (auto &it : this->get_objects()) {
-		builder << it.first << " -> " << it.second.str() << std::endl;
-	}
+    for (auto &it : this->get_objects()) {
+        builder << it.first << " -> " << it.second.str() << std::endl;
+    }
 
-	return builder.str();
+    return builder.str();
 }
 
 } // namespace nyan
--- nyan/meta_info.h
+++ nyan/meta_info.h
@@ -17,28 +17,70 @@ namespace nyan {
  */
 class MetaInfo {
 public:
-	using obj_info_t = std::unordered_map<fqon_t, ObjectInfo>;
+    using obj_info_t = std::unordered_map<fqon_t, ObjectInfo>;
 
-	MetaInfo() = default;
-	~MetaInfo() = default;
+    MetaInfo() = default;
+    ~MetaInfo() = default;
 
-	ObjectInfo &add_object(const fqon_t &name, ObjectInfo &&obj);
+    /**
+     * Add metadata information for an object.
+     *
+     * @param name Identifier of the object.
+     * @param obj_info ObjectInfo with metadata information.
+     *
+     * @return The stored metadata information object.
+     */
+    ObjectInfo &add_object(const fqon_t &name, ObjectInfo &&obj_info);
 
-	const obj_info_t &get_objects() const;
+    /**
+     * Get the all metadata information objects stored in the database.
+     *
+     * @return Map of metadata information objects by object identifier.
+     */
+    const obj_info_t &get_objects() const;
 
-	ObjectInfo *get_object(const fqon_t &name);
-	const ObjectInfo *get_object(const fqon_t &name) const;
+    /**
+     * Get the the metadata information object for an object.
+     *
+     * @param name Identifier of the object.
+     *
+     * @return ObjectInfo with metadata information if the object is
+     *     in the database, else nullptr.
+     */
+    ObjectInfo *get_object(const fqon_t &name);
 
-	bool has_object(const fqon_t &name) const;
+    /**
+     * Get the the metadata information object for an object.
+     *
+     * @param name Identifier of the object.
+     *
+     * @return ObjectInfo with metadata information if the object is
+     *     in the database, else nullptr.
+     */
+    const ObjectInfo *get_object(const fqon_t &name) const;
 
-	std::string str() const;
+    /**
+     * Check if an object is in the database.
+     *
+     * @param name Identifier of the object.
+     *
+     * @return true if the object is in the database, else false.
+     */
+    bool has_object(const fqon_t &name) const;
+
+    /**
+     * Get a string representation of all metadata information objects.
+     *
+     * @return String representation of all metadata information objects.
+     */
+    std::string str() const;
 
 protected:
-	/**
-	 * Location and type information for the objects.
-	 * This is for displaying error messages and line information.
-	 */
-	obj_info_t object_info;
+    /**
+     * Location and type information for the objects.
+     * This is for displaying error messages and line information.
+     */
+    obj_info_t object_info;
 };
 
 } // namespace nyan
