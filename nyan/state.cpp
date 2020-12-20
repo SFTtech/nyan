9122237c2d58f960ffba5fae91808e31486c85e6
heinezen <heinezen@hotmail.de>
2020-12-20 06:05:16 +0100

    refactor: Add comments to state.h.

--- nyan/state.cpp
+++ nyan/state.cpp
@@ -14,55 +14,55 @@
 namespace nyan {
 
 State::State(const std::shared_ptr<State> &previous_state)
-	:
-	previous_state{previous_state} {}
+    :
+    previous_state{previous_state} {}
 
 
 State::State()
-	:
-	previous_state{nullptr} {}
+    :
+    previous_state{nullptr} {}
 
 
 const std::shared_ptr<ObjectState> *State::get(const fqon_t &fqon) const {
-	auto it = this->objects.find(fqon);
-	if (it != std::end(this->objects)) {
-		return &it->second;
-	}
-	else {
-		return nullptr;
-	}
+    auto it = this->objects.find(fqon);
+    if (it != std::end(this->objects)) {
+        return &it->second;
+    }
+    else {
+        return nullptr;
+    }
 }
 
 
 ObjectState &State::add_object(const fqon_t &name, std::shared_ptr<ObjectState> &&obj) {
-	if (unlikely(this->previous_state != nullptr)) {
-		throw InternalError{"can't add new object in state that is not initial."};
-	}
+    if (unlikely(this->previous_state != nullptr)) {
+        throw InternalError{"can't add new object in state that is not initial."};
+    }
 
-	auto ins = this->objects.insert({name, std::move(obj)});
+    auto ins = this->objects.insert({name, std::move(obj)});
 
-	if (not ins.second) {
-		throw InternalError{"added an already-known object to the state!"};
-	}
+    if (not ins.second) {
+        throw InternalError{"added an already-known object to the state!"};
+    }
 
-	return *ins.first->second;
+    return *ins.first->second;
 }
 
 
 void State::update(std::shared_ptr<State> &&source_state) {
-	// update this state with all objects from the source state
-	// -> move all objects from the sourcestate into this one.
-	for (auto &it : source_state.get()->objects) {
-		auto search = this->objects.find(it.first);
-		if (search != std::end(this->objects)) {
-			search->second = std::move(it.second);
-		}
-		else {
-			this->objects.insert(
-				{std::move(it.first), std::move(it.second)}
-			);
-		}
-	}
+    // update this state with all objects from the source state
+    // -> move all objects from the sourcestate into this one.
+    for (auto &it : source_state.get()->objects) {
+        auto search = this->objects.find(it.first);
+        if (search != std::end(this->objects)) {
+            search->second = std::move(it.second);
+        }
+        else {
+            this->objects.insert(
+                {std::move(it.first), std::move(it.second)}
+            );
+        }
+    }
 }
 
 
@@ -70,53 +70,53 @@ const std::shared_ptr<ObjectState> &State::copy_object(const fqon_t &name,
                                                        order_t t,
                                                        std::shared_ptr<View> &origin) {
 
-	// last known state of the object
-	const std::shared_ptr<ObjectState> &source = origin->get_raw(name, t);
+    // last known state of the object
+    const std::shared_ptr<ObjectState> &source = origin->get_raw(name, t);
 
-	if (not source) {
-		throw InternalError{"object copy source not found"};
-	}
+    if (not source) {
+        throw InternalError{"object copy source not found"};
+    }
 
-	// check if the object already is in this state
-	auto it = this->objects.find(name);
-	if (it == std::end(this->objects)) {
-		// if not, copy the source object into this state
-		return this->objects.emplace(
-			name,
-			source->copy()
-		).first->second;
-	}
-	else {
-		// else, no need to copy, the object is already in this state
-		return it->second;
-	}
+    // check if the object already is in this state
+    auto it = this->objects.find(name);
+    if (it == std::end(this->objects)) {
+        // if not, copy the source object into this state
+        return this->objects.emplace(
+            name,
+            source->copy()
+        ).first->second;
+    }
+    else {
+        // else, no need to copy, the object is already in this state
+        return it->second;
+    }
 }
 
 
 const std::shared_ptr<State> &State::get_previous_state() const {
-	return this->previous_state;
+    return this->previous_state;
 }
 
 
 const std::unordered_map<fqon_t, std::shared_ptr<ObjectState>> &
 State::get_objects() const {
-	return this->objects;
+    return this->objects;
 }
 
 
 std::string State::str() const {
-	std::ostringstream builder;
+    std::ostringstream builder;
 
-	builder << "State:" << std::endl;
+    builder << "State:" << std::endl;
 
-	size_t i = 0;
-	for (auto &it : this->objects) {
-		builder << "object " << i << ":" << std::endl
-		        << it.first << " => " << it.second->str() << std::endl;
-		i += 1;
-	}
+    size_t i = 0;
+    for (auto &it : this->objects) {
+        builder << "object " << i << ":" << std::endl
+                << it.first << " => " << it.second->str() << std::endl;
+        i += 1;
+    }
 
-	return builder.str();
+    return builder.str();
 }
 
 } // namespace nyan
--- nyan/state.h
+++ nyan/state.h
@@ -21,55 +21,86 @@ class View;
  */
 class State {
 public:
-	State(const std::shared_ptr<State> &previous_state);
+    State(const std::shared_ptr<State> &previous_state);
 
-	State();
+    State();
 
-	/**
-	 * Get the object with given name in this state only.
-	 */
-	const std::shared_ptr<ObjectState> *get(const fqon_t &fqon) const;
+    /**
+     * Get an object state for a given object identifier name.
+     *
+     * @param fqon Identifier of the object.
+     *
+     * @return Shared pointer to the object state if it exists, else nullptr.
+     */
+    const std::shared_ptr<ObjectState> *get(const fqon_t &fqon) const;
 
-	/**
-	 * Add an object to the state.
-	 * This can only be done for the initial state, i.e. there's no previous state.
-	 * Why? The database must be filled at some point.
-	 */
-	ObjectState &add_object(const fqon_t &name, std::shared_ptr<ObjectState> &&obj);
+    /**
+     * Add an object state to the database state. This can only be done for the initial
+     * state, i.e. there's no previous state. Why? The database must be filled
+     * at some point.
+     *
+     * @param fqon Identifier of the object.
+     * @param obj Shared pointer to the state of the object.
+     *
+     * @return ObjectState that was added to the state.
+     */
+    ObjectState &add_object(const fqon_t &name, std::shared_ptr<ObjectState> &&obj);
 
-	/**
-	 * Add and potentially replace the objects in the storage from the other state.
-	 */
-	void update(std::shared_ptr<State> &&obj);
+    /**
+     * Add and potentially replace the object states of this state by
+     * object states from another database state.
+     *
+     * @param Shared pointer to the other database state.
+     */
+    void update(std::shared_ptr<State> &&obj);
 
-	/**
-	 * Copy an object from origin to this state.
-	 * If it is in this state already, don't copy it.
-	 */
-	const std::shared_ptr<ObjectState> &copy_object(const fqon_t &name,
-	                                                order_t t,
-	                                                std::shared_ptr<View> &origin);
+    /**
+     * Copy an object state from an origin view to this state.
+     * If it is in this state already, don't copy it.
+     *
+     * @param name Identifier of the object.
+     * @param t Time for the object state is retrieved.
+     * @param origin View from which the object state is copied.
+     *
+     * @return Shared pointer to the object state.
+     */
+    const std::shared_ptr<ObjectState> &copy_object(const fqon_t &name,
+                                                    order_t t,
+                                                    std::shared_ptr<View> &origin);
 
-	/**
-	 * Return the previous state.
-	 * The ptr contains nullptr if this is an initial state.
-	 */
-	const std::shared_ptr<State> &get_previous_state() const;
+    /**
+     * Get the previous database state.
+     *
+     * @return Shared pointer to the previous database state,
+     *     nullptr if this is the initial state.
+     */
+    const std::shared_ptr<State> &get_previous_state() const;
 
-	/**
-	 * Return the objects stored in this state.
-	 */
-	const std::unordered_map<fqon_t, std::shared_ptr<ObjectState>> &
-	get_objects() const;
+    /**
+     * Return the object states stored in this state.
+     *
+     * @return Map of shared pointers to object states by object identifier.
+     */
+    const std::unordered_map<fqon_t, std::shared_ptr<ObjectState>> &
+    get_objects() const;
 
-	/**
-	 * String representation of this state.
-	 */
-	std::string str() const;
+    /**
+     * Get the string representation of this state.
+     *
+     * @return String representation of this state.
+     */
+    std::string str() const;
 
 private:
-	std::unordered_map<fqon_t, std::shared_ptr<ObjectState>> objects;
-	std::shared_ptr<State> previous_state;
+    /**
+     * Object states in the database state.
+     */
+    std::unordered_map<fqon_t, std::shared_ptr<ObjectState>> objects;
+
+    /**
+     * Previous state.
+     */
+    std::shared_ptr<State> previous_state;
 };
 
 } // namespace nyan
