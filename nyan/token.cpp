8ba7e7a635381b43af6aa5fdf50a6bca1e985839
heinezen <heinezen@hotmail.de>
2020-12-20 06:28:04 +0100

    refactor: Add comments to token.h.

--- nyan/token.cpp
+++ nyan/token.cpp
@@ -14,78 +14,78 @@ namespace nyan {
 Token::Token(const std::shared_ptr<File> &file,
              int line, int line_offset, int length,
              token_type type)
-	:
-	location{file, line, line_offset, length},
-	type{type} {}
+    :
+    location{file, line, line_offset, length},
+    type{type} {}
 
 
 Token::Token(const std::shared_ptr<File> &file,
              int line, int line_offset, int length,
              token_type type, const std::string &value)
-	:
-	location{file, line, line_offset, length},
-	type{type},
-	value{value} {}
+    :
+    location{file, line, line_offset, length},
+    type{type},
+    value{value} {}
 
 
 Token::Token()
-	:
-	type{token_type::INVALID} {}
+    :
+    type{token_type::INVALID} {}
 
 
 const std::string &Token::get() const {
-	return this->value;
+    return this->value;
 }
 
 
 std::string Token::str() const {
-	std::ostringstream builder;
-	builder << "(" << this->location.get_line() << ":"
-	        << this->location.get_line_offset() << ": "
-	        << token_type_str(this->type);
-	if (this->value.size() > 0) {
-		builder << " '" << this->value << "'";
-	}
-	builder << ")";
-	return builder.str();
+    std::ostringstream builder;
+    builder << "(" << this->location.get_line() << ":"
+            << this->location.get_line_offset() << ": "
+            << token_type_str(this->type);
+    if (this->value.size() > 0) {
+        builder << " '" << this->value << "'";
+    }
+    builder << ")";
+    return builder.str();
 }
 
 
 bool Token::exists() const {
-	return this->get().size() > 0;
+    return this->get().size() > 0;
 }
 
 
 bool Token::is_endmarker() const {
-	switch (this->type) {
-	case token_type::ENDFILE:
-	case token_type::ENDLINE:
-		return true;
+    switch (this->type) {
+    case token_type::ENDFILE:
+    case token_type::ENDLINE:
+        return true;
 
-	case token_type::INVALID:
-		throw InternalError{"invalid token used"};
+    case token_type::INVALID:
+        throw InternalError{"invalid token used"};
 
-	default:
-		return false;
-	}
+    default:
+        return false;
+    }
 }
 
 
 bool Token::is_content() const {
-	switch (this->type) {
-	case token_type::FLOAT:
-	case token_type::ID:
-	case token_type::INF:
-	case token_type::INT:
-	case token_type::STRING:
-		return true;
+    switch (this->type) {
+    case token_type::FLOAT:
+    case token_type::ID:
+    case token_type::INF:
+    case token_type::INT:
+    case token_type::STRING:
+        return true;
 
-	case token_type::INVALID:
-		throw InternalError{"invalid token used"};
+    case token_type::INVALID:
+        throw InternalError{"invalid token used"};
 
-	default:
-		return false;
-	}
+    default:
+        return false;
+    }
 }
 
 } // namespace nyan
--- nyan/token.h
+++ nyan/token.h
@@ -15,142 +15,188 @@ class File;
  * Available token types.
  */
 enum class token_type {
-	AS,
-	AT,
-	BANG,
-	COLON,
-	COMMA,
-	DEDENT,
-	DOT,
-	ENDFILE,
-	ENDLINE,
-	ELLIPSIS,
-	FLOAT,
-	FROM,
-	ID,
-	IMPORT,
-	INDENT,
-	INF,
-	INT,
-	INVALID,
-	LANGLE,
-	LBRACE,
-	LBRACKET,
-	LPAREN,
-	OPERATOR,
-	PASS,
-	RANGLE,
-	RBRACE,
-	RBRACKET,
-	RPAREN,
-	STRING,
+    AS,
+    AT,
+    BANG,
+    COLON,
+    COMMA,
+    DEDENT,
+    DOT,
+    ENDFILE,
+    ENDLINE,
+    ELLIPSIS,
+    FLOAT,
+    FROM,
+    ID,
+    IMPORT,
+    INDENT,
+    INF,
+    INT,
+    INVALID,
+    LANGLE,
+    LBRACE,
+    LBRACKET,
+    LPAREN,
+    OPERATOR,
+    PASS,
+    RANGLE,
+    RBRACE,
+    RBRACKET,
+    RPAREN,
+    STRING,
 };
 
 /**
  * Known bracket types.
  */
 enum class bracket_type {
-	PAREN,
-	ANGLE,
-	BRACKET,
-	BRACE,
+    PAREN,
+    ANGLE,
+    BRACKET,
+    BRACE,
 };
 
 
 /**
- * Text representations of the token types.
- * Oh you, c++, such a good and comfort language.
+ * Get the string representation of a given token type.
+ *
+ * @param type Token type.
+ *
+ * @return String representation of the token type.
  */
 constexpr const char *token_type_str(token_type type) {
-	using namespace std::string_literals;
+    using namespace std::string_literals;
 
-	switch (type) {
-	case token_type::AS:             return "as";
-	case token_type::AT:             return "@";
-	case token_type::BANG:           return "!";
-	case token_type::COLON:          return "colon";
-	case token_type::COMMA:          return "comma";
-	case token_type::DEDENT:         return "dedentation";
-	case token_type::DOT:            return "dot";
-	case token_type::ELLIPSIS:       return "ellipsis";
-	case token_type::ENDFILE:        return "end of file";
-	case token_type::ENDLINE:        return "end of line";
-	case token_type::FLOAT:          return "float";
-	case token_type::FROM:           return "from";
-	case token_type::ID:             return "identifier";
-	case token_type::IMPORT:         return "import";
-	case token_type::INDENT:         return "indentation";
-	case token_type::INF:            return "inf";
-	case token_type::INT:            return "int";
-	case token_type::INVALID:        return "invalid";
-	case token_type::LANGLE:         return "'<'";
-	case token_type::LBRACE:         return "'{'";
-	case token_type::LBRACKET:       return "'['";
-	case token_type::LPAREN:         return "'('";
-	case token_type::OPERATOR:       return "operator";
-	case token_type::PASS:           return "pass";
-	case token_type::RANGLE:         return "'>'";
-	case token_type::RBRACE:         return "'}'";
-	case token_type::RBRACKET:       return "']'";
-	case token_type::RPAREN:         return "')'";
-	case token_type::STRING:         return "string";
-	}
+    switch (type) {
+    case token_type::AS:             return "as";
+    case token_type::AT:             return "@";
+    case token_type::BANG:           return "!";
+    case token_type::COLON:          return "colon";
+    case token_type::COMMA:          return "comma";
+    case token_type::DEDENT:         return "dedentation";
+    case token_type::DOT:            return "dot";
+    case token_type::ELLIPSIS:       return "ellipsis";
+    case token_type::ENDFILE:        return "end of file";
+    case token_type::ENDLINE:        return "end of line";
+    case token_type::FLOAT:          return "float";
+    case token_type::FROM:           return "from";
+    case token_type::ID:             return "identifier";
+    case token_type::IMPORT:         return "import";
+    case token_type::INDENT:         return "indentation";
+    case token_type::INF:            return "inf";
+    case token_type::INT:            return "int";
+    case token_type::INVALID:        return "invalid";
+    case token_type::LANGLE:         return "'<'";
+    case token_type::LBRACE:         return "'{'";
+    case token_type::LBRACKET:       return "'['";
+    case token_type::LPAREN:         return "'('";
+    case token_type::OPERATOR:       return "operator";
+    case token_type::PASS:           return "pass";
+    case token_type::RANGLE:         return "'>'";
+    case token_type::RBRACE:         return "'}'";
+    case token_type::RBRACKET:       return "']'";
+    case token_type::RPAREN:         return "')'";
+    case token_type::STRING:         return "string";
+    }
 
-	return "unhandled token_type";
+    return "unhandled token_type";
 }
 
 
 /**
- * Return if the given token type requires a payload storage.
+ * Check if the given token type requires a payload storage.
  * If not, then the token type is already enough information.
+ *
+ * @param type Token type.
+ *
+ * @return true if the token type requires a payload, else false.
  */
 constexpr bool token_needs_payload(token_type type) {
 
-	switch (type) {
-	case token_type::FLOAT:
-	case token_type::ID:
-	case token_type::INF:
-	case token_type::INT:
-	case token_type::OPERATOR:
-	case token_type::STRING:
-		return true;
-	default:
-		return false;
-	}
+    switch (type) {
+    case token_type::FLOAT:
+    case token_type::ID:
+    case token_type::INF:
+    case token_type::INT:
+    case token_type::OPERATOR:
+    case token_type::STRING:
+        return true;
+    default:
+        return false;
+    }
 }
 
 
 /**
- * These are spit out by the nyan lexer.
+ * Tokens are generated by the nyan lexer.
  */
 class Token {
 public:
-	Token();
-	Token(const std::shared_ptr<File> &file,
-	      int line,
-	      int line_offset,
-	      int length,
-	      token_type type);
-	Token(const std::shared_ptr<File> &file,
-	      int line,
-	      int line_offset,
-	      int length,
-	      token_type type,
-	      const std::string &value);
-	~Token() = default;
+    Token();
+    Token(const std::shared_ptr<File> &file,
+          int line,
+          int line_offset,
+          int length,
+          token_type type);
+    Token(const std::shared_ptr<File> &file,
+          int line,
+          int line_offset,
+          int length,
+          token_type type,
+          const std::string &value);
+    ~Token() = default;
 
-	std::string str() const;
-	bool exists() const;
-	bool is_endmarker() const;
-	bool is_content() const;
+    /**
+     * Check if the token has a payload.
+     *
+     * @return true if a payload exists, else false.
+     */
+    bool exists() const;
 
-	const std::string &get() const;
+    /**
+     * Check if the token is an endmarker (EOL or EOF).
+     *
+     * @return true if the token is an endmarker, else false.
+     */
+    bool is_endmarker() const;
 
-	Location location;
-	token_type type;
+    /**
+     * Check if the token type is used for storing content.
+     * This can be member values or object references.
+     *
+     * @return true if the token type is used for content, else false.
+     */
+    bool is_content() const;
+
+    /**
+     * Get the token payload.
+     *
+     * @return String of the token payload.
+     */
+    const std::string &get() const;
+
+    /**
+     * Get the string representation of the token.
+     *
+     * @return String representation of the token.
+     */
+    std::string str() const;
+
+    /**
+     * Location of the token in a file.
+     */
+    Location location;
+
+    /**
+     * Token type.
+     */
+    token_type type;
 
 protected:
-	std::string value;
+    /**
+     * Token payload.
+     */
+    std::string value;
 };
 
 } // namespace nyan
