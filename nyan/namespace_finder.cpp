6756dc1259607d7f9f9f5f30ac1d4d096c88d097
heinezen <heinezen@hotmail.de>
2020-12-20 01:23:18 +0100

    refactor: Add comments to namespace_finder.h.

--- nyan/namespace_finder.cpp
+++ nyan/namespace_finder.cpp
@@ -11,50 +11,50 @@
 namespace nyan {
 
 NamespaceFinder::NamespaceFinder(AST &&ast)
-	:
-	ast{std::move(ast)} {}
+    :
+    ast{std::move(ast)} {}
 
 
 void NamespaceFinder::add_import(const Namespace &import) {
-	this->imports.insert(import);
+    this->imports.insert(import);
 }
 
 
 void NamespaceFinder::add_alias(const Token &alias,
                                 const Namespace &destination) {
 
-	const std::string &search = alias.get();
+    const std::string &search = alias.get();
 
-	if (this->aliases.find(search) != std::end(this->aliases)) {
-		throw NameError{alias, "redefinition of namespace alias", search};
-	}
+    if (this->aliases.find(search) != std::end(this->aliases)) {
+        throw NameError{alias, "redefinition of namespace alias", search};
+    }
 
-	this->aliases.insert({search, destination});
+    this->aliases.insert({search, destination});
 }
 
 
 bool NamespaceFinder::check_conflict(const std::string &name) const {
-	return (this->aliases.find(name) != std::end(this->aliases) or
-	        this->imports.find(Namespace{name}) != std::end(this->imports));
+    return (this->aliases.find(name) != std::end(this->aliases) or
+            this->imports.find(Namespace{name}) != std::end(this->imports));
 }
 
 
 fqon_t NamespaceFinder::expand_alias(const IDToken &name) const {
-	if (unlikely(not name.exists())) {
-		throw InternalError{"tried expanding alias on empty id token"};
-	}
+    if (unlikely(not name.exists())) {
+        throw InternalError{"tried expanding alias on empty id token"};
+    }
 
-	// only the first component can be an alias.
-	const std::string &first = name.get_components()[0].get();
+    // only the first component can be an alias.
+    const std::string &first = name.get_components()[0].get();
 
-	auto it = this->aliases.find(first);
-	if (it != std::end(this->aliases)) {
-		// alias found. now expand it.
-		return it->second.combine(name, 1);
-	}
+    auto it = this->aliases.find(first);
+    if (it != std::end(this->aliases)) {
+        // alias found. now expand it.
+        return it->second.combine(name, 1);
+    }
 
-	// no alias found. basically return the input name.
-	return Namespace{name}.to_fqon();
+    // no alias found. basically return the input name.
+    return Namespace{name}.to_fqon();
 }
 
 
@@ -62,59 +62,59 @@ fqon_t NamespaceFinder::find(const Namespace &current,
                              const IDToken &search,
                              const MetaInfo &typedb) const {
 
-	if (unlikely(not search.exists())) {
-		throw InternalError{"tried to find namespace for empty id"};
-	}
+    if (unlikely(not search.exists())) {
+        throw InternalError{"tried to find namespace for empty id"};
+    }
 
-	Namespace search_base{current};
+    Namespace search_base{current};
 
-	fqon_t result;
-	// go towards the root namespace
-	// to search for the matching object name
-	while (true) {
-		result = search_base.combine(search);
-		if (typedb.has_object(result)) {
-			return result;
-		}
+    fqon_t result;
+    // go towards the root namespace
+    // to search for the matching object name
+    while (true) {
+        result = search_base.combine(search);
+        if (typedb.has_object(result)) {
+            return result;
+        }
 
-		// if the search base is exhausted, do alias expansion.
-		if (search_base.empty()) {
-			result = this->expand_alias(search);
+        // if the search base is exhausted, do alias expansion.
+        if (search_base.empty()) {
+            result = this->expand_alias(search);
 
-			if (not typedb.has_object(result)) {
-				throw NameError{search, "unknown name", search.str()};
-			}
+            if (not typedb.has_object(result)) {
+                throw NameError{search, "unknown name", search.str()};
+            }
 
-			return result;
-		}
+            return result;
+        }
 
-		search_base.pop_last();
-	}
+        search_base.pop_last();
+    }
 }
 
 
 const AST &NamespaceFinder::get_ast() const {
-	return this->ast;
+    return this->ast;
 }
 
 
 std::string NamespaceFinder::str() const {
-	std::ostringstream builder;
-	builder << "NamespaceFinder knows:" << std::endl
-	        << "= aliases:" << std::endl;
+    std::ostringstream builder;
+    builder << "NamespaceFinder knows:" << std::endl
+            << "= aliases:" << std::endl;
 
-	for (auto &it : this->aliases) {
-		builder << " * " << it.first
-		        << " => " << it.second.str() << std::endl;
-	}
+    for (auto &it : this->aliases) {
+        builder << " * " << it.first
+                << " => " << it.second.str() << std::endl;
+    }
 
-	builder << "= imports:" << std::endl;
+    builder << "= imports:" << std::endl;
 
-	for (auto &it : this->imports) {
-		builder << " * " << it.str() << std::endl;
-	}
+    for (auto &it : this->imports) {
+        builder << " * " << it.str() << std::endl;
+    }
 
-	return builder.str();
+    return builder.str();
 }
 
 } // namespace nyan
--- nyan/namespace_finder.h
+++ nyan/namespace_finder.h
@@ -13,47 +13,92 @@
 namespace nyan {
 
 class NamespaceFinder {
-	/**
-	 * Lookup map to find the namespace for an alias.
-	 */
-	using namespace_alias_t = std::unordered_map<std::string, Namespace>;
+    /**
+     * Lookup map to find the namespace for an alias.
+     */
+    using namespace_alias_t = std::unordered_map<std::string, Namespace>;
 
-	/**
-	 * In the current namespace, namespaces in here were imported.
-	 */
-	using namespace_available_t = std::unordered_set<Namespace>;
-
-public:
-	NamespaceFinder(AST &&ast);
-
-	void add_import(const Namespace &ns);
-	void add_alias(const Token &alias, const Namespace &destination);
-
-	/**
-	 * check for naming conflict of an object.
-	 * returns true if the name conflicts.
-	 */
-	bool check_conflict(const std::string &name) const;
-
-	/** Expand the id for an alias. */
-	fqon_t expand_alias(const IDToken &name) const;
-
-	/** Search for a fqon in a given namespace and below */
-	fqon_t find(const Namespace &current,
-	            const IDToken &search,
-	            const MetaInfo &typedb) const;
-
-	/** Return the AST of this namespace */
-	const AST &get_ast() const;
-
-	std::string str() const;
+    /**
+     * In the current namespace, namespaces in here were imported.
+     */
+    using namespace_available_t = std::unordered_set<Namespace>;
 
 public:
-	AST ast;
+    NamespaceFinder(AST &&ast);
 
-	namespace_available_t imports;
+    /**
+     * Imports another namespace.
+     *
+     * @param ns Namespace that should be imported.
+     */
+    void add_import(const Namespace &ns);
 
-	namespace_alias_t aliases;
+    /**
+     * Imports another namespace by using an alias.
+     *
+     * @param alias Token with the name of the alias.
+     * @param ns Namespace that should be imported.
+     */
+    void add_alias(const Token &alias, const Namespace &destination);
+
+    /**
+     * Check if a name conflicts wth other names in the namespace, i.e. the
+     * name is already used.
+     *
+     * @return true if the name is already used, else false.
+     */
+    bool check_conflict(const std::string &name) const;
+
+    /**
+     * Get the identifier of a namespace associated with an alias.
+     *
+     * @param name IDToken with an alias as its first component.
+     *
+     * @return Identifier of the namespace.
+     */
+    fqon_t expand_alias(const IDToken &name) const;
+
+    /**
+     * Search for the object identifier of an object reference
+     * in a given namespace.
+     *
+     * @param name IDToken with an object reference.
+     *
+     * @return Identifier of the object.
+     */
+    fqon_t find(const Namespace &current,
+                const IDToken &search,
+                const MetaInfo &typedb) const;
+
+    /**
+     * Get the AST (abstract syntax tree) of this namespace.
+     *
+     * @return AST of this namespace.
+     */
+    const AST &get_ast() const;
+
+    /**
+     * Get the string representation of this namespace.
+     *
+     * @return String representation of this namespace.
+     */
+    std::string str() const;
+
+public:
+    /**
+     * Abstract syntax tree of the namespace.
+     */
+    AST ast;
+
+    /**
+     * Directly imported namespaces.
+     */
+    namespace_available_t imports;
+
+    /**
+     * Namespaces imported by alias.
+     */
+    namespace_alias_t aliases;
 };
 
 } // namespace nyan
