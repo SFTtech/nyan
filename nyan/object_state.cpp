5ed8e63d2c3f546d0320c58d3ec43cbc04fa714c
heinezen <heinezen@hotmail.de>
2020-12-20 03:44:29 +0100

    refactor: Add comments to object_state.h.

--- nyan/object_state.cpp
+++ nyan/object_state.cpp
@@ -15,135 +15,135 @@ namespace nyan {
 
 
 ObjectState::ObjectState(std::deque<fqon_t> &&parents)
-	:
-	parents{std::move(parents)} {}
+    :
+    parents{std::move(parents)} {}
 
 
 void ObjectState::apply(const std::shared_ptr<ObjectState> &mod,
                         const ObjectInfo &mod_info,
                         ObjectChanges &tracker) {
 
-	const auto &inher_changes = mod_info.get_inheritance_change();
-	if (inher_changes.size() > 0) {
-		for (auto &change : inher_changes) {
+    const auto &inher_changes = mod_info.get_inheritance_change();
+    if (inher_changes.size() > 0) {
+        for (auto &change : inher_changes) {
 
-			bool parent_exists = (
-				std::find(
-					std::begin(this->parents),
-					std::end(this->parents),
-					change.get_target()
-				) != std::end(this->parents)
-			);
+            bool parent_exists = (
+                std::find(
+                    std::begin(this->parents),
+                    std::end(this->parents),
+                    change.get_target()
+                ) != std::end(this->parents)
+            );
 
-			// only add the parent if it does not exist.
-			// maybe we may want to relocate it in the future?
-			if (not parent_exists) {
-				switch (change.get_type()) {
-				case inher_change_t::ADD_FRONT:
-					this->parents.push_front(change.get_target());
-					tracker.add_parent(change.get_target());
-					break;
-				case inher_change_t::ADD_BACK:
-					this->parents.push_back(change.get_target());
-					tracker.add_parent(change.get_target());
-					break;
-				default:
-					throw InternalError{"unsupported inheritance change type"};
-				}
-			}
-		}
-	}
+            // only add the parent if it does not exist.
+            // maybe we may want to relocate it in the future?
+            if (not parent_exists) {
+                switch (change.get_type()) {
+                case inher_change_t::ADD_FRONT:
+                    this->parents.push_front(change.get_target());
+                    tracker.add_parent(change.get_target());
+                    break;
+                case inher_change_t::ADD_BACK:
+                    this->parents.push_back(change.get_target());
+                    tracker.add_parent(change.get_target());
+                    break;
+                default:
+                    throw InternalError{"unsupported inheritance change type"};
+                }
+            }
+        }
+    }
 
-	// change each member in this object by the member of the patch.
-	// other->members: maps memberid_t name => Member
-	for (auto &it : mod->members) {
-		auto search = this->members.find(it.first);
-		if (search == std::end(this->members)) {
-			// copy the member from the modification object,
-			// if it is a patch.
-			// that way a child object without the member
-			// can get the modifications.
-			if (likely(mod_info.is_patch())) {
-				this->members.emplace(it.first, it.second);
-			}
-			else {
-				throw InternalError{
-					"a non-patch tried to change a nonexisting member"
-				};
-			}
-		}
-		else {
-			search->second.apply(it.second);
-		}
+    // change each member in this object by the member of the patch.
+    // other->members: maps memberid_t name => Member
+    for (auto &it : mod->members) {
+        auto search = this->members.find(it.first);
+        if (search == std::end(this->members)) {
+            // copy the member from the modification object,
+            // if it is a patch.
+            // that way a child object without the member
+            // can get the modifications.
+            if (likely(mod_info.is_patch())) {
+                this->members.emplace(it.first, it.second);
+            }
+            else {
+                throw InternalError{
+                    "a non-patch tried to change a nonexisting member"
+                };
+            }
+        }
+        else {
+            search->second.apply(it.second);
+        }
 
-		// TODO optimization: we could now calculate the resulting value!
-		// TODO: invalidate value cache with the change tracker
-	}
+        // TODO optimization: we could now calculate the resulting value!
+        // TODO: invalidate value cache with the change tracker
+    }
 }
 
 
 std::shared_ptr<ObjectState> ObjectState::copy() const {
-	return std::make_shared<ObjectState>(*this);
+    return std::make_shared<ObjectState>(*this);
 }
 
 
 const std::deque<fqon_t> &ObjectState::get_parents() const {
-	return this->parents;
+    return this->parents;
 }
 
 
 bool ObjectState::has(const memberid_t &name) const {
-	return this->members.find(name) != std::end(this->members);
+    return this->members.find(name) != std::end(this->members);
 }
 
 
 Member *ObjectState::get(const memberid_t &name) {
-	auto it = this->members.find(name);
-	if (it == std::end(this->members)) {
-		return nullptr;
-	}
-	return &it->second;
+    auto it = this->members.find(name);
+    if (it == std::end(this->members)) {
+        return nullptr;
+    }
+    return &it->second;
 }
 
 
 // Thanks C++, always redundancy free!
 const Member *ObjectState::get(const memberid_t &name) const {
-	auto it = this->members.find(name);
-	if (it == std::end(this->members)) {
-		return nullptr;
-	}
-	return &it->second;
+    auto it = this->members.find(name);
+    if (it == std::end(this->members)) {
+        return nullptr;
+    }
+    return &it->second;
 }
 
 
 const std::unordered_map<memberid_t, Member> &ObjectState::get_members() const {
-	return this->members;
+    return this->members;
 }
 
 
 std::string ObjectState::str() const {
-	std::ostringstream builder;
+    std::ostringstream builder;
 
-	builder << "ObjectState("
-	        << util::strjoin(", ", this->parents)
-	        << ")"
-	        << std::endl;
+    builder << "ObjectState("
+            << util::strjoin(", ", this->parents)
+            << ")"
+            << std::endl;
 
-	if (this->members.size() == 0) {
-		builder << "    [no members]" << std::endl;
-	}
+    if (this->members.size() == 0) {
+        builder << "    [no members]" << std::endl;
+    }
 
-	for (auto &it : this->members) {
-		builder << "    " << it.first
-		        << " -> " << it.second.str() << std::endl;
-	}
+    for (auto &it : this->members) {
+        builder << "    " << it.first
+                << " -> " << it.second.str() << std::endl;
+    }
 
-	return builder.str();
+    return builder.str();
 }
 
 
 void ObjectState::set_members(std::unordered_map<memberid_t, Member> &&members) {
-	this->members = std::move(members);
+    this->members = std::move(members);
 }
 
 } // namespace nyan
--- nyan/object_state.h
+++ nyan/object_state.h
@@ -18,49 +18,98 @@ class ObjectInfo;
  * Single object state storage.
  */
 class ObjectState {
-	friend class Database;
+    friend class Database;
 public:
-	/**
-	 * Creation of an initial object state.
-	 */
-	ObjectState(std::deque<fqon_t> &&parents);
+    /**
+     * Creation of an initial object state.
+     */
+    ObjectState(std::deque<fqon_t> &&parents);
 
-	/**
-	 * Patch application.
-	 */
-	void apply(const std::shared_ptr<ObjectState> &other,
-	           const ObjectInfo &mod_info,
-	           ObjectChanges &tracker);
+    /**
+     * Apply changes to this object with a patch.
+     *
+     * @param mod Shared pointer to the patch modifying this object.
+     * @param mod_info Metadata information object of the patch.
+     * @param tracker Tracker for changes to this object.
+     */
+    void apply(const std::shared_ptr<ObjectState> &mod,
+               const ObjectInfo &mod_info,
+               ObjectChanges &tracker);
 
-	std::shared_ptr<ObjectState> copy() const;
+    /**
+     * Copy this object state.
+     *
+     * @return Shared pointer to the copy of the object state.
+     */
+    std::shared_ptr<ObjectState> copy() const;
 
-	const std::deque<fqon_t> &get_parents() const;
+    /**
+     * Get the parents of this object.
+     *
+     * @return Double-ended queue with identifiers for the parent objects.
+     */
+    const std::deque<fqon_t> &get_parents() const;
 
-	bool has(const memberid_t &name) const;
-	Member *get(const memberid_t &name);
-	const Member *get(const memberid_t &name) const;
-	const std::unordered_map<memberid_t, Member> &get_members() const;
+    /**
+     * Check if the object has a member with a given identifier.
+     *
+     * @param name Identifier of the member.
+     *
+     * @return true if the object has a member with the identifier, else false.
+     */
+    bool has(const memberid_t &name) const;
 
-	std::string str() const;
+    /**
+     * Get the pointer to a member with a given identifier.
+     *
+     * @param name Identifier of the member.
+     *
+     * @return Pointer to the member if the object has this member, else nullptr.
+     */
+    Member *get(const memberid_t &name);
+
+    /**
+     * Get the pointer to a member with a given identifier.
+     *
+     * @param name Identifier of the member.
+     *
+     * @return Pointer to the member object if the object has this member, else nullptr.
+     */
+    const Member *get(const memberid_t &name) const;
+
+    /**
+     * Get the members of this object.
+     *
+     * @return Map of the member objects by member identifer.
+     */
+    const std::unordered_map<memberid_t, Member> &get_members() const;
+
+    /**
+     * Get the string representation of this object state.
+     *
+     * @return String representation of this object state.
+     */
+    std::string str() const;
 
 private:
-	/**
-	 * Replace the member map.
-	 * Used for creating initial object states.
-	 */
-	void set_members(std::unordered_map<memberid_t, Member> &&members);
+    /**
+     * Replace the member map. Used for creating initial object states.
+     *
+     * @param members Map of the member objects by member identifer.
+     */
+    void set_members(std::unordered_map<memberid_t, Member> &&members);
 
-	/**
-	 * Parent objects.
-	 */
-	std::deque<fqon_t> parents;
+    /**
+     * Parent objects.
+     */
+    std::deque<fqon_t> parents;
 
-	/**
-	 * Member storage.
-	 */
-	std::unordered_map<memberid_t, Member> members;
+    /**
+     * Member objects storage.
+     */
+    std::unordered_map<memberid_t, Member> members;
 
-	// The object location is stored in the metainfo-database.
+    // The object location is stored in the metainfo-database.
 };
 
 } // namespace nyan
