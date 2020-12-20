d48c87312d9e0c5844beae1772b184b91582c698
heinezen <heinezen@hotmail.de>
2020-12-20 04:25:30 +0100

    refactor: Add comments to object.h.

--- nyan/object.cpp
+++ nyan/object.cpp
@@ -26,231 +26,231 @@
 namespace nyan {
 
 Object::Object(const fqon_t &name, const std::shared_ptr<View> &origin)
-	:
-	origin{origin},
-	name{name} {}
+    :
+    origin{origin},
+    name{name} {}
 
 
 Object::~Object() = default;
 
 
 const fqon_t &Object::get_name() const {
-	return this->name;
+    return this->name;
 }
 
 
 const std::shared_ptr<View> &Object::get_view() const {
-	return this->origin;
+    return this->origin;
 }
 
 
 ValueHolder Object::get_value(const memberid_t &member, order_t t) const {
-	return this->calculate_value(member, t);
+    return this->calculate_value(member, t);
 }
 
 
 value_int_t Object::get_int(const memberid_t &member, order_t t) const {
-	return this->get_number<Int>(member, t);
+    return this->get_number<Int>(member, t);
 }
 
 
 value_float_t Object::get_float(const memberid_t &member, order_t t) const {
-	return this->get_number<Float>(member, t);
+    return this->get_number<Float>(member, t);
 }
 
 
 const std::string &Object::get_text(const memberid_t &member, order_t t) const {
-	return *this->get<Text>(member, t);
+    return *this->get<Text>(member, t);
 }
 
 
 bool Object::get_bool(const memberid_t &member, order_t t) const {
-	return *this->get<Boolean>(member, t);
+    return *this->get<Boolean>(member, t);
 }
 
 
 const set_t &Object::get_set(const memberid_t &member, order_t t) const {
-	return this->get<Set>(member, t)->get();
+    return this->get<Set>(member, t)->get();
 }
 
 
 const ordered_set_t &Object::get_orderedset(const memberid_t &member, order_t t) const {
-	return this->get<OrderedSet>(member, t)->get();
+    return this->get<OrderedSet>(member, t)->get();
 }
 
 
 const std::string &Object::get_file(const memberid_t &member, order_t t) const {
-	return this->get<Filename>(member, t)->get();
+    return this->get<Filename>(member, t)->get();
 }
 
 
 Object Object::get_object(const memberid_t &member, order_t t) const {
-	return *this->get<Object>(member, t);
+    return *this->get<Object>(member, t);
 }
 
 
 template <>
 std::shared_ptr<Object> Object::get<Object>(const memberid_t &member, order_t t) const {
-	auto obj_val = this->get<ObjectValue>(member, t);
-	fqon_t fqon = obj_val->get();
-	std::shared_ptr<Object> ret = std::make_shared<Object>(Object::Restricted{},
-	                                                       fqon, this->origin);
-	return ret;
+    auto obj_val = this->get<ObjectValue>(member, t);
+    fqon_t fqon = obj_val->get();
+    std::shared_ptr<Object> ret = std::make_shared<Object>(Object::Restricted{},
+                                                           fqon, this->origin);
+    return ret;
 }
 
 
 ValueHolder Object::calculate_value(const memberid_t &member, order_t t) const {
-	using namespace std::string_literals;
+    using namespace std::string_literals;
 
-	// TODO: don't allow calculating values for patches?
-	// it's impossible as they may have members without =
+    // TODO: don't allow calculating values for patches?
+    // it's impossible as they may have members without =
 
-	// get references to all parentobject-states
-	std::vector<std::shared_ptr<ObjectState>> parents;
+    // get references to all parentobject-states
+    std::vector<std::shared_ptr<ObjectState>> parents;
 
-	const std::vector<fqon_t> &linearization = this->get_linearized(t);
+    const std::vector<fqon_t> &linearization = this->get_linearized(t);
 
-	// find the last value assigning with =
-	// it sets the base value where we apply the modifications then
-	size_t defined_by = 0;
+    // find the last value assigning with =
+    // it sets the base value where we apply the modifications then
+    size_t defined_by = 0;
 
-	const Value *base_value = nullptr;
-	for (auto &obj : linearization) {
-		parents.push_back(this->origin->get_raw(obj, t));
-		const ObjectState *obj_raw = parents.back().get();
-		const Member *obj_member = obj_raw->get(member);
-		// if the object has the member, check if it's the =
-		if (obj_member != nullptr) {
-			if (obj_member->get_operation() == nyan_op::ASSIGN) {
-				base_value = &obj_member->get_value();
-				break;
-			}
-		}
-		defined_by += 1;
-	}
+    const Value *base_value = nullptr;
+    for (auto &obj : linearization) {
+        parents.push_back(this->origin->get_raw(obj, t));
+        const ObjectState *obj_raw = parents.back().get();
+        const Member *obj_member = obj_raw->get(member);
+        // if the object has the member, check if it's the =
+        if (obj_member != nullptr) {
+            if (obj_member->get_operation() == nyan_op::ASSIGN) {
+                base_value = &obj_member->get_value();
+                break;
+            }
+        }
+        defined_by += 1;
+    }
 
-	// no operator = was found for this member
-	// -> no parent assigned a value.
-	// errors in the data files are detected at load time already.
-	if (unlikely(defined_by >= linearization.size() or base_value == nullptr)) {
-		throw MemberNotFoundError{this->name, member};
-	}
+    // no operator = was found for this member
+    // -> no parent assigned a value.
+    // errors in the data files are detected at load time already.
+    if (unlikely(defined_by >= linearization.size() or base_value == nullptr)) {
+        throw MemberNotFoundError{this->name, member};
+    }
 
-	// if this object defines the value, no aggregation is needed.
-	if (defined_by == 0) {
-		return base_value->copy();
-	}
+    // if this object defines the value, no aggregation is needed.
+    if (defined_by == 0) {
+        return base_value->copy();
+    }
 
-	// create a working copy of the value
-	ValueHolder result = base_value->copy();
+    // create a working copy of the value
+    ValueHolder result = base_value->copy();
 
-	// walk back and apply the value changes
-	while (true) {
-		const Member *change = parents[defined_by]->get(member);
-		if (change != nullptr) {
-			result->apply(*change);
-		}
-		if (defined_by == 0) {
-			break;
-		}
-		defined_by -= 1;
-	}
+    // walk back and apply the value changes
+    while (true) {
+        const Member *change = parents[defined_by]->get(member);
+        if (change != nullptr) {
+            result->apply(*change);
+        }
+        if (defined_by == 0) {
+            break;
+        }
+        defined_by -= 1;
+    }
 
-	return result;
+    return result;
 }
 
 
 const std::deque<fqon_t> &Object::get_parents(order_t t) const {
-	return this->get_raw(t)->get_parents();
+    return this->get_raw(t)->get_parents();
 }
 
 
 bool Object::has(const memberid_t &member, order_t t) const {
-	// TODO: cache?
+    // TODO: cache?
 
-	const std::vector<fqon_t> &lin = this->get_linearized(t);
+    const std::vector<fqon_t> &lin = this->get_linearized(t);
 
-	for (auto &obj : lin) {
-		if (this->origin->get_raw(obj, t)->get(member) != nullptr) {
-			return true;
-		}
-	}
+    for (auto &obj : lin) {
+        if (this->origin->get_raw(obj, t)->get(member) != nullptr) {
+            return true;
+        }
+    }
 
-	return false;
+    return false;
 }
 
 
 bool Object::extends(fqon_t other_fqon, order_t t) const {
-	if (this->name == other_fqon) {
-		return true;
-	}
+    if (this->name == other_fqon) {
+        return true;
+    }
 
-	// TODO cache?
+    // TODO cache?
 
-	auto &linearization = this->get_linearized(t);
+    auto &linearization = this->get_linearized(t);
 
-	for (auto &obj : linearization) {
-		if (obj == other_fqon) {
-			return true;
-		}
-	}
+    for (auto &obj : linearization) {
+        if (obj == other_fqon) {
+            return true;
+        }
+    }
 
-	return false;
+    return false;
 }
 
 
 const ObjectInfo &Object::get_info() const {
-	if (unlikely(not this->name.size())) {
-		throw InvalidObjectError{};
-	}
+    if (unlikely(not this->name.size())) {
+        throw InvalidObjectError{};
+    }
 
-	const ObjectInfo *ret = this->origin->get_database().get_info().get_object(this->get_name());
-	if (unlikely(ret == nullptr)) {
-		throw InternalError{"object info unavailable for object handle"};
-	}
-	return *ret;
+    const ObjectInfo *ret = this->origin->get_database().get_info().get_object(this->get_name());
+    if (unlikely(ret == nullptr)) {
+        throw InternalError{"object info unavailable for object handle"};
+    }
+    return *ret;
 }
 
 
 bool Object::is_patch() const {
-	// must be a patch from the beginning of time!
-	return this->get_info().is_patch();
+    // must be a patch from the beginning of time!
+    return this->get_info().is_patch();
 }
 
 
 const fqon_t *Object::get_target() const {
-	const PatchInfo *patch_info = this->get_info().get_patch().get();
-	if (unlikely(patch_info == nullptr)) {
-		return nullptr;
-	}
-	return &patch_info->get_target();
+    const PatchInfo *patch_info = this->get_info().get_patch().get();
+    if (unlikely(patch_info == nullptr)) {
+        return nullptr;
+    }
+    return &patch_info->get_target();
 }
 
 
 const std::vector<fqon_t> &Object::get_linearized(order_t t) const {
-	if (unlikely(not this->name.size())) {
-		throw InvalidObjectError{};
-	}
+    if (unlikely(not this->name.size())) {
+        throw InvalidObjectError{};
+    }
 
-	return this->origin->get_linearization(this->name, t);
+    return this->origin->get_linearization(this->name, t);
 }
 
 std::shared_ptr<ObjectNotifier>
 Object::subscribe(const update_cb_t &callback) {
-	if (unlikely(not this->name.size())) {
-		throw InvalidObjectError{};
-	}
+    if (unlikely(not this->name.size())) {
+        throw InvalidObjectError{};
+    }
 
-	return this->origin->create_notifier(this->name, callback);
+    return this->origin->create_notifier(this->name, callback);
 }
 
 
 const std::shared_ptr<ObjectState> &Object::get_raw(order_t t) const {
-	if (unlikely(not this->name.size())) {
-		throw InvalidObjectError{};
-	}
+    if (unlikely(not this->name.size())) {
+        throw InvalidObjectError{};
+    }
 
-	return this->origin->get_raw(this->name, t);
+    return this->origin->get_raw(this->name, t);
 }
 
 } // namespace nyan
--- nyan/object.h
+++ nyan/object.h
@@ -31,167 +31,314 @@ class View;
  * Handle for accessing a nyan object independent of time.
  */
 class Object {
-	friend class View;
+    friend class View;
 protected:
-	/**
-	 * Create a nyan-object handle. This is never invoked by the user,
-	 * as handles are generated internally and then handed over.
-	 */
-	Object(const fqon_t &name, const std::shared_ptr<View> &origin);
-	class Restricted {};
+    /**
+     * Create a nyan-object handle. This is never invoked by the user,
+     * as handles are generated internally and then handed over.
+     */
+    Object(const fqon_t &name, const std::shared_ptr<View> &origin);
+    class Restricted {};
 
 public:
-	/**
-	 * Default constructor for an invalid nyan::Object.
-	 */
-	Object() = default;
+    /**
+     * Default constructor for an invalid nyan::Object.
+     */
+    Object() = default;
 
-	// This constructor is public, but can't be invoked since the Restricted
-	// class is not available. We use this to be able to invoke make_shared
-	// within this class, but not outside of it.
-	Object(Object::Restricted, const fqon_t &name, const std::shared_ptr<View> &origin)
-		: Object(name, origin) {};
-	~Object();
+    // This constructor is public, but can't be invoked since the Restricted
+    // class is not available. We use this to be able to invoke make_shared
+    // within this class, but not outside of it.
+    Object(Object::Restricted, const fqon_t &name, const std::shared_ptr<View> &origin)
+        : Object(name, origin) {};
+    ~Object();
 
-	/**
-	 * Return the fully-qualified object name.
-	 */
-	const fqon_t &get_name() const;
+    /**
+     * Get the identifier of this object (fully-qualified object name).
+     *
+     * @return Identifier of this object.
+     */
+    const fqon_t &get_name() const;
 
-	/**
-	 * Return the view this object was retrieved from.
-	 */
-	const std::shared_ptr<View> &get_view() const;
+    /**
+     * Get the view this object was created in.
+     *
+     * @return View of this object.
+     */
+    const std::shared_ptr<View> &get_view() const;
 
-	/**
-	 * Get a calculated member value.
-	 */
-	ValueHolder get_value(const memberid_t &member, order_t t=LATEST_T) const;
+    /**
+     * Get the calculated member value for a given member at a given time.
+     *
+     * @param member Identifier of the member.
+     * @param t Time for which we want to calculate the value.
+     *
+     * @return ValueHolder with the value of the member.
+     */
+    ValueHolder get_value(const memberid_t &member, order_t t=LATEST_T) const;
 
-	/**
-	 * Invokes the get_value function and then does a cast.
-	 * There's a special variant for T=nyan::Object which creates
-	 * an object handle.
-	 *
-	 * TODO: either return a stored variant reference or the shared ptr of the holder
-	 */
-	template <typename T>
-	std::shared_ptr<T> get(const memberid_t &member, order_t t=LATEST_T) const;
+    /**
+     * Get the calculated member value for a given member at a given time.
+     *
+     * Invokes the get_value function and then does a cast to type T.
+     * There's a special variant for T=nyan::Object which creates
+     * an object handle.
+     *
+     * TODO: either return a stored variant reference or the shared ptr of the holder
+     *
+     * @tparam Type the value is casted to.
+     *
+     * @param member Identifier of the member.
+     * @param t Time for which we want to calculate the value.
+     *
+     * @return Shared pointer with the value of the member.
+     */
+    template <typename T>
+    std::shared_ptr<T> get(const memberid_t &member, order_t t=LATEST_T) const;
 
-	template<typename T, typename ret=typename T::storage_type>
-	ret get_number(const memberid_t &member, order_t t=LATEST_T) const;
+    /**
+     * Get the calculated member value for a given member at a given time.
+     *
+     * Casts to a number type T.
+     *
+     * @tparam Number type the value is casted to.
+     * @tparam Return type of the value.
+     *
+     * @param member Identifier of the member.
+     * @param t Time for which we want to calculate the value.
+     *
+     * @return Value of the member with type \p ret.
+     */
+    template<typename T, typename ret=typename T::storage_type>
+    ret get_number(const memberid_t &member, order_t t=LATEST_T) const;
 
-	value_int_t get_int(const memberid_t &member, order_t t=LATEST_T) const;
+    /**
+     * Get the calculated member value for a given member at a given time.
+     *
+     * Casts to int.
+     *
+     * @param member Identifier of the member.
+     * @param t Time for which we want to calculate the value.
+     *
+     * @return Value of the member.
+     */
+    value_int_t get_int(const memberid_t &member, order_t t=LATEST_T) const;
 
-	value_float_t get_float(const memberid_t &member, order_t t=LATEST_T) const;
+    /**
+     * Get the calculated member value for a given member at a given time.
+     *
+     * Casts to float.
+     *
+     * @param member Identifier of the member.
+     * @param t Time for which the value is calculated.
+     *
+     * @return Value of the member.
+     */
+    value_float_t get_float(const memberid_t &member, order_t t=LATEST_T) const;
 
-	const std::string &get_text(const memberid_t &member, order_t t=LATEST_T) const;
+    /**
+     * Get the calculated member value for a given member at a given time.
+     *
+     * Casts to std::string.
+     *
+     * @param member Identifier of the member.
+     * @param t Time for which the value is calculated.
+     *
+     * @return Value of the member.
+     */
+    const std::string &get_text(const memberid_t &member, order_t t=LATEST_T) const;
 
-	bool get_bool(const memberid_t &member, order_t t=LATEST_T) const;
+    /**
+     * Get the calculated member value for a given member at a given time.
+     *
+     * Casts to bool.
+     *
+     * @param member Identifier of the member.
+     * @param t Time for which the value is calculated.
+     *
+     * @return Value of the member.
+     */
+    bool get_bool(const memberid_t &member, order_t t=LATEST_T) const;
 
-	const set_t &get_set(const memberid_t &member, order_t t=LATEST_T) const;
+    /**
+     * Get the calculated member value for a given member at a given time.
+     *
+     * Casts to std::unordered_set.
+     *
+     * @param member Identifier of the member.
+     * @param t Time for which the value is calculated.
+     *
+     * @return Value of the member.
+     */
+    const set_t &get_set(const memberid_t &member, order_t t=LATEST_T) const;
 
-	const ordered_set_t &get_orderedset(const memberid_t &member, order_t t=LATEST_T) const;
+    /**
+     * Get the calculated member value for a given member at a given time.
+     *
+     * Casts to nyan::OrderedSet.
+     *
+     * @param member Identifier of the member.
+     * @param t Time for which the value is calculated.
+     *
+     * @return Value of the member.
+     */
+    const ordered_set_t &get_orderedset(const memberid_t &member, order_t t=LATEST_T) const;
 
-	const std::string &get_file(const memberid_t &member, order_t t=LATEST_T) const;
+    /**
+     * Get the calculated member value for a given member at a given time.
+     *
+     * Casts to std::string.
+     *
+     * @param member Identifier of the member.
+     * @param t Time for which the value is calculated.
+     *
+     * @return Value of the member.
+     */
+    const std::string &get_file(const memberid_t &member, order_t t=LATEST_T) const;
 
-	Object get_object(const memberid_t &fqon, order_t t=LATEST_T) const;
+    /**
+     * Get the calculated member value for a given member at a given time.
+     *
+     * Returns an Object.
+     *
+     * @param member Identifier of the member.
+     * @param t Time for which the value is calculated.
+     *
+     * @return Value of the member.
+     */
+    Object get_object(const memberid_t &fqon, order_t t=LATEST_T) const;
 
-	/**
-	 * Return the parents of the object.
-	 */
-	const std::deque<fqon_t> &get_parents(order_t t=LATEST_T) const;
+    /**
+     * Get the parents of this object at a given time.
+     *
+     * @param t Time for which the parents are returned.
+     *
+     * @return Double-linked queue containing the parents of this object.
+     */
+    const std::deque<fqon_t> &get_parents(order_t t=LATEST_T) const;
 
-	/**
-	 * Test if this object has a member of given name.
-	 */
-	bool has(const memberid_t &member, order_t t=LATEST_T) const;
+    /**
+     * Test if this object has a member with a given name at a given time.
+     *
+     * @param member Identifier of the member.
+     * @param t Time for which the member existence is checked.
+     *
+     * @return true if the member exists for this object, else false.
+     */
+    bool has(const memberid_t &member, order_t t=LATEST_T) const;
 
-	/**
-	 * Test if this object is a child of the given parent.
-	 * Returns true if other_fqon equals this object or any
-	 * of its (transitive) parents.
-	 */
-	bool extends(fqon_t other_fqon, order_t t=LATEST_T) const;
+    /**
+     * Check if this object is a child of the given parent at a given time.
+     *
+     * @param other_fqon Identifier of the suspected parent object.
+     * @param t Time for which the relationship is checked.
+     *
+     * @return true if the parent's identifier equals this object's
+     *     identifier or that of any of its (transitive) parents,
+     *     else false
+     */
+    bool extends(fqon_t other_fqon, order_t t=LATEST_T) const;
 
-	/**
-	 * Return the object metadata.
-	 */
-	const ObjectInfo &get_info() const;
+    /**
+     * Get the metadata information object for this object.
+     *
+     * @return Metadata information object for this object.
+     */
+    const ObjectInfo &get_info() const;
 
-	/**
-	 * Check if this object is a patch.
-	 * Currently, a non-patch can never become a new patch.
-	 */
-	bool is_patch() const;
+    /**
+     * Check if this object is a patch.
+     *
+     * @return true if this is a patch, else false.
+     */
+    bool is_patch() const;
 
-	/**
-	 * Get the patch target. Returns nullptr if the object is not a patch.
-	 */
-	const fqon_t *get_target() const;
+    /**
+     * Get the identifier of the patch target.
+     *
+     * @return Identifier of the patch target if this object is a
+     *     patch, else nullptr.
+     */
+    const fqon_t *get_target() const;
 
-	/**
-	 * Return the linearization of this object and its parent objects.
-	 */
-	const std::vector<fqon_t> &get_linearized(order_t t=LATEST_T) const;
+    /**
+     * Return the C3 linearization of this object at a given time.
+     *
+     * @param t Time for which the C3 linearization is calculated.
+     *
+     * @return C3 linearization of this object.
+     */
+    const std::vector<fqon_t> &get_linearized(order_t t=LATEST_T) const;
 
-	/**
-	 * Register a function that will be called when this object changes in its current view.
-	 * It is triggered when a patch is applied on this object
-	 * or a parent object.
-	 * The callback is registered in this object's view and will be fired as long
-	 * as the returned ObjectNotifier was not deleted.
-	 */
-	std::shared_ptr<ObjectNotifier> subscribe(const update_cb_t &callback);
+    /**
+     * Register a function that will be called when this object changes in its current view.
+     * It is triggered when a patch is applied on this object or a parent object.
+     * The callback is registered in this object's view and will be fired as long
+     * as the returned ObjectNotifier was not deleted.
+     *
+     * @param callback Callback function that is executed when a patch
+     *     is applied to this object or a parent object.
+     *
+     * @return Shared pointer to the ObjectNotifier.
+     */
+    std::shared_ptr<ObjectNotifier> subscribe(const update_cb_t &callback);
 
 protected:
 
-	/**
-	 * Return the object state for a given time.
-	 */
-	const std::shared_ptr<ObjectState> &get_raw(order_t t=LATEST_T) const;
+    /**
+     * Get the object state at a given time.
+     *
+     * @param t Point in time for which the object state is retrieved.
+     *
+     * @return Shared pointer to the object state.
+     */
+    const std::shared_ptr<ObjectState> &get_raw(order_t t=LATEST_T) const;
 
-	/**
-	 * Calculate a member value of this object.
-	 * This performs tree traversal for value calculations.
-	 */
-	ValueHolder calculate_value(const memberid_t &member, order_t t=LATEST_T) const;
+    /**
+     * Get the calculated member value for a given member at a given time.
+     *
+     * @param member Identifier of the member.
+     * @param t Time for which we want to calculate the value.
+     *
+     * @return ValueHolder with the value of the member.
+     */
+    ValueHolder calculate_value(const memberid_t &member, order_t t=LATEST_T) const;
 
-	/**
-	 * View the object was created from.
-	 */
-	std::shared_ptr<View> origin;
+    /**
+     * View the object was created from.
+     */
+    std::shared_ptr<View> origin;
 
-	/**
-	 * The name of this object.
-	 */
-	fqon_t name;
+    /**
+     * The name of this object.
+     */
+    fqon_t name;
 };
 
 
 // TODO: use concepts...
 template <typename T>
 std::shared_ptr<T> Object::get(const memberid_t &member, order_t t) const {
-	std::shared_ptr<Value> value = this->get_value(member, t).get_ptr();
-	auto ret = std::dynamic_pointer_cast<T>(value);
+    std::shared_ptr<Value> value = this->get_value(member, t).get_ptr();
+    auto ret = std::dynamic_pointer_cast<T>(value);
 
-	if (not ret) {
-		throw MemberTypeError{
-			this->name,
-			member,
-			util::typestring(value.get()),
-			util::typestring<T>()
-		};
-	}
+    if (not ret) {
+        throw MemberTypeError{
+            this->name,
+            member,
+            util::typestring(value.get()),
+            util::typestring<T>()
+        };
+    }
 
-	return ret;
+    return ret;
 }
 
 
 // TODO: use concepts...
 template<typename T, typename ret>
 ret Object::get_number(const memberid_t &member, order_t t) const {
-	return *this->get<T>(member, t);
+    return *this->get<T>(member, t);
 }
 
 
