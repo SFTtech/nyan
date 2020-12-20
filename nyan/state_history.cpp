0f79321e2e70fadb34ddb4ede2beb8ff1243ae59
heinezen <heinezen@hotmail.de>
2020-12-20 05:51:04 +0100

    refactor: Add comments to state_history.h.

--- nyan/state_history.cpp
+++ nyan/state_history.cpp
@@ -12,76 +12,76 @@ namespace nyan {
 
 StateHistory::StateHistory(const std::shared_ptr<Database> &base) {
 
-	// create new empty state to work on at the beginning.
-	this->insert(
-		std::make_shared<State>(base->get_state()),
-		DEFAULT_T
-	);
+    // create new empty state to work on at the beginning.
+    this->insert(
+        std::make_shared<State>(base->get_state()),
+        DEFAULT_T
+    );
 }
 
 const std::shared_ptr<State> &StateHistory::get_state(order_t t) const {
-	return this->history.at(t);
+    return this->history.at(t);
 }
 
 
 const std::shared_ptr<State> &StateHistory::get_state_before(order_t t) const {
-	return this->history.at(t)->get_previous_state();
+    return this->history.at(t)->get_previous_state();
 }
 
 
 const std::shared_ptr<State> *StateHistory::get_state_exact(order_t t) const {
-	return this->history.at_exact(t);
+    return this->history.at_exact(t);
 }
 
 
 const std::shared_ptr<ObjectState> *StateHistory::get_obj_state(const fqon_t &fqon, order_t t) const {
-	// get the object history
-	const ObjectHistory *obj_history = this->get_obj_history(fqon);
+    // get the object history
+    const ObjectHistory *obj_history = this->get_obj_history(fqon);
 
-	// object isn't recorded in this state history
-	if (obj_history == nullptr) {
-		return nullptr;
-	}
+    // object isn't recorded in this state history
+    if (obj_history == nullptr) {
+        return nullptr;
+    }
 
-	std::optional<order_t> order = obj_history->last_change_before(t);
+    std::optional<order_t> order = obj_history->last_change_before(t);
 
-	if (not order) {
-		// the change is earlier than what is recorded in this history.
-		return nullptr;
-	}
+    if (not order) {
+        // the change is earlier than what is recorded in this history.
+        return nullptr;
+    }
 
-	// now we know the time in history the object was changed
-	const std::shared_ptr<State> *state = this->history.at_exact(*order);
-	if (unlikely(state == nullptr)) {
-		throw InternalError{"no history record at change point"};
-	}
+    // now we know the time in history the object was changed
+    const std::shared_ptr<State> *state = this->history.at_exact(*order);
+    if (unlikely(state == nullptr)) {
+        throw InternalError{"no history record at change point"};
+    }
 
-	const std::shared_ptr<ObjectState> *obj_state = (*state)->get(fqon);
-	if (unlikely(state == nullptr)) {
-		throw InternalError{"object state not found at change point"};
-	}
+    const std::shared_ptr<ObjectState> *obj_state = (*state)->get(fqon);
+    if (unlikely(state == nullptr)) {
+        throw InternalError{"object state not found at change point"};
+    }
 
-	return obj_state;
+    return obj_state;
 }
 
 
 void StateHistory::insert(std::shared_ptr<State> &&new_state, order_t t) {
 
-	// record the changes.
-	for (const auto &it : new_state->get_objects()) {
-		ObjectHistory &obj_history = this->get_create_obj_history(it.first);
-		obj_history.insert_change(t);
-	}
+    // record the changes.
+    for (const auto &it : new_state->get_objects()) {
+        ObjectHistory &obj_history = this->get_create_obj_history(it.first);
+        obj_history.insert_change(t);
+    }
 
-	// drop all later changes
-	this->history.insert_drop(t, std::move(new_state));
+    // drop all later changes
+    this->history.insert_drop(t, std::move(new_state));
 }
 
 
 void StateHistory::insert_linearization(std::vector<fqon_t> &&ins, order_t t) {
-	const auto &obj = ins.at(0);
+    const auto &obj = ins.at(0);
 
-	this->get_create_obj_history(obj).linearizations.insert_drop(t, std::move(ins));
+    this->get_create_obj_history(obj).linearizations.insert_drop(t, std::move(ins));
 }
 
 
@@ -89,24 +89,24 @@ const std::vector<fqon_t> &
 StateHistory::get_linearization(const fqon_t &obj, order_t t,
                                 const MetaInfo &meta_info) const {
 
-	const ObjectHistory *obj_hist = this->get_obj_history(obj);
-	if (obj_hist != nullptr) {
-		if (not obj_hist->linearizations.empty()) {
-			auto ret = obj_hist->linearizations.at_find(t);
+    const ObjectHistory *obj_hist = this->get_obj_history(obj);
+    if (obj_hist != nullptr) {
+        if (not obj_hist->linearizations.empty()) {
+            auto ret = obj_hist->linearizations.at_find(t);
 
-			if (ret != nullptr) {
-				return *ret;
-			}
-		}
-	}
+            if (ret != nullptr) {
+                return *ret;
+            }
+        }
+    }
 
-	// otherwise, the lin is only stored in the database.
-	const ObjectInfo *obj_info = meta_info.get_object(obj);
-	if (unlikely(obj_info == nullptr)) {
-		throw InternalError{"object not found in metainfo"};
-	}
+    // otherwise, the lin is only stored in the database.
+    const ObjectInfo *obj_info = meta_info.get_object(obj);
+    if (unlikely(obj_info == nullptr)) {
+        throw InternalError{"object not found in metainfo"};
+    }
 
-	return obj_info->get_linearization();
+    return obj_info->get_linearization();
 }
 
 
@@ -114,7 +114,7 @@ void StateHistory::insert_children(const fqon_t &obj,
                                    std::unordered_set<fqon_t> &&ins,
                                    order_t t) {
 
-	this->get_create_obj_history(obj).children.insert_drop(t, std::move(ins));
+    this->get_create_obj_history(obj).children.insert_drop(t, std::move(ins));
 }
 
 
@@ -122,61 +122,61 @@ const std::unordered_set<fqon_t> &
 StateHistory::get_children(const fqon_t &obj, order_t t,
                            const MetaInfo &meta_info) const {
 
-	// first try the obj_history
-	const ObjectHistory *obj_hist = this->get_obj_history(obj);
-	if (obj_hist != nullptr) {
-		if (not obj_hist->children.empty()) {
-			auto ret = obj_hist->children.at_find(t);
+    // first try the obj_history
+    const ObjectHistory *obj_hist = this->get_obj_history(obj);
+    if (obj_hist != nullptr) {
+        if (not obj_hist->children.empty()) {
+            auto ret = obj_hist->children.at_find(t);
 
-			if (ret != nullptr) {
-				return *ret;
-			}
-		}
-	}
+            if (ret != nullptr) {
+                return *ret;
+            }
+        }
+    }
 
-	// otherwise, the lin is only stored in the database.
-	const ObjectInfo *obj_info = meta_info.get_object(obj);
-	if (unlikely(obj_info == nullptr)) {
-		throw InternalError{"object not found in metainfo"};
-	}
+    // otherwise, the lin is only stored in the database.
+    const ObjectInfo *obj_info = meta_info.get_object(obj);
+    if (unlikely(obj_info == nullptr)) {
+        throw InternalError{"object not found in metainfo"};
+    }
 
-	return obj_info->get_children();
+    return obj_info->get_children();
 }
 
 
 ObjectHistory *StateHistory::get_obj_history(const fqon_t &obj) {
-	auto it = this->object_obj_hists.find(obj);
-	if (it != std::end(this->object_obj_hists)) {
-		return &it->second;
-	}
-	else {
-		return nullptr;
-	}
+    auto it = this->object_obj_hists.find(obj);
+    if (it != std::end(this->object_obj_hists)) {
+        return &it->second;
+    }
+    else {
+        return nullptr;
+    }
 }
 
 
 const ObjectHistory *StateHistory::get_obj_history(const fqon_t &obj) const {
-	auto it = this->object_obj_hists.find(obj);
-	if (it != std::end(this->object_obj_hists)) {
-		return &it->second;
-	}
-	else {
-		return nullptr;
-	}
+    auto it = this->object_obj_hists.find(obj);
+    if (it != std::end(this->object_obj_hists)) {
+        return &it->second;
+    }
+    else {
+        return nullptr;
+    }
 }
 
 
 ObjectHistory &StateHistory::get_create_obj_history(const fqon_t &obj) {
-	auto it = this->object_obj_hists.find(obj);
-	if (it != std::end(this->object_obj_hists)) {
-		return it->second;
-	}
-	else {
-		// create new obj_history entry.
-		return this->object_obj_hists.emplace(
-			obj, ObjectHistory{}
-		).first->second;
-	}
+    auto it = this->object_obj_hists.find(obj);
+    if (it != std::end(this->object_obj_hists)) {
+        return it->second;
+    }
+    else {
+        // create new obj_history entry.
+        return this->object_obj_hists.emplace(
+            obj, ObjectHistory{}
+        ).first->second;
+    }
 }
 
 } // namespace nyan
--- nyan/state_history.h
+++ nyan/state_history.h
@@ -23,48 +23,136 @@ class State;
  */
 class StateHistory {
 public:
-	StateHistory(const std::shared_ptr<Database> &base);
+    StateHistory(const std::shared_ptr<Database> &base);
 
-	/** return the state for t or if t doesn't exist, the latest state before that */
-	const std::shared_ptr<State> &get_state(order_t t) const;
+    /**
+     * Get the latest database state at or after a given time.
+     *
+     * @param t Time for which the state is retrieved.
+     *
+     * @return Shared pointer to State at time \p t if it exists, else the next state after that.
+     */
+    const std::shared_ptr<State> &get_state(order_t t) const;
 
-	/** return the latest state before t, that is if t exists, the previous one. */
-	const std::shared_ptr<State> &get_state_before(order_t t) const;
+    /**
+     * Get the latest database state before a given time.
+     *
+     * @param t Time for which the state is retrieved.
+     *
+     * @return Shared pointer to State before time \p t .
+     */
+    const std::shared_ptr<State> &get_state_before(order_t t) const;
 
-	/** return the state at exactly t, which is nullptr if there's no state at t */
-	const std::shared_ptr<State> *get_state_exact(order_t t) const;
+    /**
+     * Get the database state at a given time.
+     *
+     * @param t Time for which the state is retrieved.
+     *
+     * @return Shared pointer to State at time \p t if it exists, else nullptr..
+     */
+    const std::shared_ptr<State> *get_state_exact(order_t t) const;
 
-	/**
-	 * find the latest object state for a given object at t.
-	 * if there's no object state at t, take the latest state before t.
-	 */
-	const std::shared_ptr<ObjectState> *get_obj_state(const fqon_t &fqon, order_t t) const;
+    /**
+     * Get an object state at a given time.
+     *
+     * @param fqon Identifier of the object.
+     * @param t Time for which the object state is retrieved.
+     *
+     * @return Shared pointer to the ObjectState at time \p t if
+     *     it exists, else the next state before that.
+     */
+    const std::shared_ptr<ObjectState> *get_obj_state(const fqon_t &fqon, order_t t) const;
 
-	void insert(std::shared_ptr<State> &&new_state, order_t t);
+    /**
+     * Record all changes of a new state in the history.
+     *
+     * @param new_state New state in the database.
+     * @param t Time of insertion.
+     */
+    void insert(std::shared_ptr<State> &&new_state, order_t t);
 
-	void insert_linearization(std::vector<fqon_t> &&ins, order_t t);
-	const std::vector<fqon_t> &get_linearization(const fqon_t &obj, order_t t,
-	                                             const MetaInfo &meta_info) const;
+    /**
+     * Record a change to the linearization of an object in its history.
+     *
+     * @param ins New linearization of the object. The first element in
+     *     the list is also the identifier of the object.
+     * @param t Time of insertion.
+     */
+    void insert_linearization(std::vector<fqon_t> &&ins, order_t t);
 
-	void insert_children(const fqon_t &obj, std::unordered_set<fqon_t> &&ins, order_t t);
-	const std::unordered_set<fqon_t> &get_children(const fqon_t &obj, order_t t,
-	                                               const MetaInfo &meta_info) const;
+    /**
+     * Get the linearization of an object at a given time.
+     *
+     * @param obj Identifier of the object.
+     * @param t Time for which the object linearization is retrieved.
+     * @param meta_info Metadata information of the database.
+     *
+     * @return C3 linearization of the object.
+     */
+    const std::vector<fqon_t> &get_linearization(const fqon_t &obj, order_t t,
+                                                 const MetaInfo &meta_info) const;
+
+    /**
+     * Record a change to the children of an object in its history.
+     *
+     * @param obj Identifier of the object.
+     * @param ins New children of the object.
+     * @param t Time of insertion.
+     */
+    void insert_children(const fqon_t &obj, std::unordered_set<fqon_t> &&ins, order_t t);
+
+    /**
+     * Get the children of an object at a given time.
+     *
+     * @param obj Identifier of the object.
+     * @param t Time for which the object children are retrieved.
+     * @param meta_info Metadata information of the database.
+     *
+     * @return List of children of the object.
+     */
+    const std::unordered_set<fqon_t> &get_children(const fqon_t &obj, order_t t,
+                                                   const MetaInfo &meta_info) const;
 
 protected:
-	ObjectHistory *get_obj_history(const fqon_t &obj);
-	const ObjectHistory *get_obj_history(const fqon_t &obj) const;
-	ObjectHistory &get_create_obj_history(const fqon_t &obj);
 
-	/**
-	 * Storage of states over time.
-	 */
-	Curve<std::shared_ptr<State>> history;
+    /**
+     * Get the object history an an object in the database.
+     *
+     * @param obj Identifier of the object.
+     *
+     * @return Pointer to the ObjectHistory of the object if it exists, else nullptr.
+     */
+    ObjectHistory *get_obj_history(const fqon_t &obj);
 
-	/**
-	 * Information history for each object.
-	 * Optimizes searches in the history.
-	 */
-	std::unordered_map<fqon_t, ObjectHistory> object_obj_hists;
+    /**
+     * Get the object history an an object in the database.
+     *
+     * @param obj Identifier of the object.
+     *
+     * @return Pointer to the ObjectHistory of the object if it exists, else nullptr.
+     */
+    const ObjectHistory *get_obj_history(const fqon_t &obj) const;
+
+    /**
+     * Get the object history an an object in the database or create it if
+     * it doesn't exist.
+     *
+     * @param obj Identifier of the object.
+     *
+     * @return Pointer to the ObjectHistory of the object.
+     */
+    ObjectHistory &get_create_obj_history(const fqon_t &obj);
+
+    /**
+     * Storage of states over time.
+     */
+    Curve<std::shared_ptr<State>> history;
+
+    /**
+     * Information history for each object.
+     * Optimizes searches in the history.
+     */
+    std::unordered_map<fqon_t, ObjectHistory> object_obj_hists;
 };
 
 
