7d5dfaac3db00377856b6a5e99585017713c2fff
heinezen <heinezen@hotmail.de>
2020-12-21 03:36:58 +0100

    refactor: Add comments to util.h.

--- nyan/util.cpp
+++ nyan/util.cpp
@@ -26,46 +26,46 @@ namespace {
 
 
 std::string symbol_name_win(const void *addr, bool require_exact_addr) {
-	// Handle to the current process
-	static HANDLE process_handle = INVALID_HANDLE_VALUE;
-	static bool initialized_symbol_handler = false;
-	static bool initialized_symbol_handler_successfully = false;
+    // Handle to the current process
+    static HANDLE process_handle = INVALID_HANDLE_VALUE;
+    static bool initialized_symbol_handler = false;
+    static bool initialized_symbol_handler_successfully = false;
 
-	// SymInitialize & SymFromAddr are, according to MSDN, not thread-safe.
-	static std::mutex sym_mutex;
-	std::lock_guard<std::mutex> sym_lock_guard{sym_mutex};
+    // SymInitialize & SymFromAddr are, according to MSDN, not thread-safe.
+    static std::mutex sym_mutex;
+    std::lock_guard<std::mutex> sym_lock_guard{sym_mutex};
 
-	// Initialize symbol handler for process, if it has not yet been initialized
-	// If we are not succesful on the first try, leave it, since MSDN says that searching for symbol files is very time consuming
-	if (!initialized_symbol_handler) {
-		initialized_symbol_handler = true;
+    // Initialize symbol handler for process, if it has not yet been initialized
+    // If we are not succesful on the first try, leave it, since MSDN says that searching for symbol files is very time consuming
+    if (!initialized_symbol_handler) {
+        initialized_symbol_handler = true;
 
-		process_handle = GetCurrentProcess();
-		initialized_symbol_handler_successfully = SymInitialize(process_handle, nullptr, TRUE);
-	}
+        process_handle = GetCurrentProcess();
+        initialized_symbol_handler_successfully = SymInitialize(process_handle, nullptr, TRUE);
+    }
 
-	if (!initialized_symbol_handler_successfully) {
-		return {};
-	}
+    if (!initialized_symbol_handler_successfully) {
+        return {};
+    }
 
-	// The magic of winapi
-	constexpr int name_buffer_size = 1024;
-	constexpr int buffer_size = sizeof(SYMBOL_INFO) + name_buffer_size * sizeof(char);
-	std::array<char, buffer_size> buffer;
+    // The magic of winapi
+    constexpr int name_buffer_size = 1024;
+    constexpr int buffer_size = sizeof(SYMBOL_INFO) + name_buffer_size * sizeof(char);
+    std::array<char, buffer_size> buffer;
 
-	SYMBOL_INFO *symbol_info = reinterpret_cast<SYMBOL_INFO*>(buffer.data());
+    SYMBOL_INFO *symbol_info = reinterpret_cast<SYMBOL_INFO*>(buffer.data());
 
-	symbol_info->SizeOfStruct = sizeof(SYMBOL_INFO);
-	symbol_info->MaxNameLen = name_buffer_size;
+    symbol_info->SizeOfStruct = sizeof(SYMBOL_INFO);
+    symbol_info->MaxNameLen = name_buffer_size;
 
-	DWORD64 symbol_offset = 0;
-	if (not SymFromAddr(process_handle, reinterpret_cast<DWORD64>(addr),
-	                    &symbol_offset, symbol_info)
-	    or (require_exact_addr and symbol_offset != 0)) {
-		return {};
-	}
+    DWORD64 symbol_offset = 0;
+    if (not SymFromAddr(process_handle, reinterpret_cast<DWORD64>(addr),
+                        &symbol_offset, symbol_info)
+        or (require_exact_addr and symbol_offset != 0)) {
+        return {};
+    }
 
-	return std::string(symbol_info->Name);
+    return std::string(symbol_info->Name);
 }
 
 
@@ -77,121 +77,121 @@ namespace nyan::util {
 
 
 std::string read_file(const std::string &filename, bool binary) {
-	std::ifstream::openmode mode = std::ifstream::in | std::ifstream::ate;
-	if (binary) {
-		mode |= std::ifstream::binary;
-	}
+    std::ifstream::openmode mode = std::ifstream::in | std::ifstream::ate;
+    if (binary) {
+        mode |= std::ifstream::binary;
+    }
 
-	std::ifstream input{filename, mode};
+    std::ifstream input{filename, mode};
 
-	if (input) {
-		std::string ret;
+    if (input) {
+        std::string ret;
 
-		// we're at the file end already because of ifstream::ate
-		ret.resize(input.tellg());
-		input.seekg(0, std::ios::beg);
-		input.read(&ret[0], ret.size());
-		input.close();
+        // we're at the file end already because of ifstream::ate
+        ret.resize(input.tellg());
+        input.seekg(0, std::ios::beg);
+        input.read(&ret[0], ret.size());
+        input.close();
 
-		return ret;
-	}
-	else {
-		std::ostringstream builder;
-		builder << "failed reading file '"
-		        << filename << "': "
-		        << strerror(errno);
-		throw FileReadError{builder.str()};
-	}
+        return ret;
+    }
+    else {
+        std::ostringstream builder;
+        builder << "failed reading file '"
+                << filename << "': "
+                << strerror(errno);
+        throw FileReadError{builder.str()};
+    }
 }
 
 
 std::string demangle(const char *symbol) {
 #ifdef _MSC_VER
-	// TODO: demangle names for MSVC; Possibly using UnDecorateSymbolName
-	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms681400(v=vs.85).aspx
-	return symbol;
+    // TODO: demangle names for MSVC; Possibly using UnDecorateSymbolName
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/ms681400(v=vs.85).aspx
+    return symbol;
 #else
-	int status;
-	char *buf = abi::__cxa_demangle(symbol, nullptr, nullptr, &status);
+    int status;
+    char *buf = abi::__cxa_demangle(symbol, nullptr, nullptr, &status);
 
-	if (status != 0) {
-		return symbol;
-	} else {
-		std::string result{buf};
-		free(buf);
-		return result;
-	}
+    if (status != 0) {
+        return symbol;
+    } else {
+        std::string result{buf};
+        free(buf);
+        return result;
+    }
 #endif
 }
 
 
 std::string addr_to_string(const void *addr) {
-	std::ostringstream out;
-	out << "[" << addr << "]";
-	return out.str();
+    std::ostringstream out;
+    out << "[" << addr << "]";
+    return out.str();
 }
 
 
 std::string symbol_name(const void *addr,
                         bool require_exact_addr, bool no_pure_addrs) {
 #if defined(_WIN32) || defined(__CYGWIN__)
-	auto symbol_name_result = symbol_name_win(addr, require_exact_addr);
+    auto symbol_name_result = symbol_name_win(addr, require_exact_addr);
 
-	if (!symbol_name_result.empty()) {
-		return symbol_name_result;
-	}
+    if (!symbol_name_result.empty()) {
+        return symbol_name_result;
+    }
 
-	return no_pure_addrs ? "" : addr_to_string(addr);
+    return no_pure_addrs ? "" : addr_to_string(addr);
 #else
-	Dl_info addr_info;
+    Dl_info addr_info;
 
-	if (dladdr(addr, &addr_info) == 0) {
-		// dladdr has... failed.
-		return no_pure_addrs ? "" : addr_to_string(addr);
-	} else {
-		size_t symbol_offset = reinterpret_cast<size_t>(addr) -
-		                       reinterpret_cast<size_t>(addr_info.dli_saddr);
+    if (dladdr(addr, &addr_info) == 0) {
+        // dladdr has... failed.
+        return no_pure_addrs ? "" : addr_to_string(addr);
+    } else {
+        size_t symbol_offset = reinterpret_cast<size_t>(addr) -
+                               reinterpret_cast<size_t>(addr_info.dli_saddr);
 
-		if (addr_info.dli_sname == nullptr or
-		    (symbol_offset != 0 and require_exact_addr)) {
+        if (addr_info.dli_sname == nullptr or
+            (symbol_offset != 0 and require_exact_addr)) {
 
-			return no_pure_addrs ? "" : addr_to_string(addr);
-		}
+            return no_pure_addrs ? "" : addr_to_string(addr);
+        }
 
-		if (symbol_offset == 0) {
-			// this is our symbol name.
-			return demangle(addr_info.dli_sname);
-		} else {
-			std::ostringstream out;
-			out << demangle(addr_info.dli_sname)
-			    << "+0x" << std::hex
-			    << symbol_offset << std::dec;
-			return out.str();
-		}
-	}
+        if (symbol_offset == 0) {
+            // this is our symbol name.
+            return demangle(addr_info.dli_sname);
+        } else {
+            std::ostringstream out;
+            out << demangle(addr_info.dli_sname)
+                << "+0x" << std::hex
+                << symbol_offset << std::dec;
+            return out.str();
+        }
+    }
 #endif
 }
 
 
 std::vector<std::string> split(const std::string &txt, char delimiter) {
-	std::vector<std::string> items;
-	// use the back inserter iterator and the templated split function.
-	split(txt, delimiter, std::back_inserter(items));
-	return items;
+    std::vector<std::string> items;
+    // use the back inserter iterator and the templated split function.
+    split(txt, delimiter, std::back_inserter(items));
+    return items;
 }
 
 
 bool ends_with(const std::string &txt, const std::string &end) {
-	if (end.size() > txt.size()) {
-		return false;
-	}
-	return std::equal(end.rbegin(), end.rend(), txt.rbegin());
+    if (end.size() > txt.size()) {
+        return false;
+    }
+    return std::equal(end.rbegin(), end.rend(), txt.rbegin());
 }
 
 
 size_t hash_combine(size_t hash1, size_t hash2) {
-	// Taken from http://www.boost.org/doc/libs/1_55_0/doc/html/hash/reference.html#boost.hash_combine
-	return hash1 ^ (hash2 + 0x9e3779b9 + ((hash1 << 6) + (hash1 >> 2)));
+    // Taken from http://www.boost.org/doc/libs/1_55_0/doc/html/hash/reference.html#boost.hash_combine
+    return hash1 ^ (hash2 + 0x9e3779b9 + ((hash1 << 6) + (hash1 >> 2)));
 }
 
 } // namespace nyan::util
--- nyan/util.h
+++ nyan/util.h
@@ -16,7 +16,12 @@ namespace nyan::util {
 
 /**
  * Read a file from the filesystem and return the contents.
- * Optionally, open it in binary mode, which will leave newlines untouched.
+ *
+ * @param filename Name of the file.
+ * @param binary If true, open the file in binary mode, which
+ *     will leave newlines untouched.
+ *
+ * @return String with the file content.
  */
 std::string read_file(const std::string &filename, bool binary=false);
 
@@ -39,7 +44,7 @@ std::string symbol_name(const void *addr, bool require_exact_addr=true, bool no_
  */
 template <typename T>
 std::string typestring() {
-	return demangle(typeid(T).name());
+    return demangle(typeid(T).name());
 }
 
 /**
@@ -47,13 +52,13 @@ std::string typestring() {
  */
 template <typename T>
 std::string typestring(const T *ptr) {
-	return demangle(typeid(*ptr).name());
+    return demangle(typeid(*ptr).name());
 }
 
 
 template <typename T>
 std::string get_container_elem(const T &in) {
-	return in;
+    return in;
 }
 
 
@@ -65,103 +70,147 @@ std::string get_container_elem(const T &in) {
  * Now, this function also features a compiler bug:
  * https://gcc.gnu.org/bugzilla/show_bug.cgi?id=59949
  * Fak u C++.
+ *
+ * @param delim Delimeter placed between the joined strings.
+ * @param container Iterable container holding the strings.
+ * @param func Function for retrieving a string from a container item.
+ *
+ * @return String containing the joined strings.
  */
 template <typename T>
 std::string strjoin(const std::string &delim,
                     const T &container,
                     const std::function<std::string(const typename T::value_type &)> func=&get_container_elem<typename T::value_type>) {
 
-	std::ostringstream builder;
+    std::ostringstream builder;
 
-	size_t cnt = 0;
-	for (auto &entry : container) {
-		if (cnt > 0) {
-			builder << delim;
-		}
+    size_t cnt = 0;
+    for (auto &entry : container) {
+        if (cnt > 0) {
+            builder << delim;
+        }
 
-		builder << func(entry);
-		cnt += 1;
-	}
+        builder << func(entry);
+        cnt += 1;
+    }
 
-	return builder.str();
+    return builder.str();
 }
 
 
 /**
  * Split a string at a delimiter, push the result back in an iterator.
  * Why doesn't the fucking standard library have std::string::split(delimiter)?
+ *
+ * @tparam ret_t Return type.
+ *
+ * @param[in]  txt String that is split.
+ * @param[in]  delimiter Delimiter char at which the string is split.
+ * @param[out] result Splitted string with type \p ret_t.
  */
 template<typename ret_t>
 void split(const std::string &txt, char delimiter, ret_t result) {
-	std::stringstream splitter;
-	splitter.str(txt);
-	std::string part;
+    std::stringstream splitter;
+    splitter.str(txt);
+    std::string part;
 
-	while (std::getline(splitter, part, delimiter)) {
-		*result = part;
-		result++;
-	}
+    while (std::getline(splitter, part, delimiter)) {
+        *result = part;
+        result++;
+    }
 }
 
 
 /**
  * Split a string at a delimiter into a vector.
- * Internally, uses the above iterator splitter.
+ * Internally, uses the iterator splitter.
+ *
+ * @param txt String that is split.
+ * @param delim Delimiter char at which the string is split.
+ *
+ * @return List of string components from the splitting.
  */
 std::vector<std::string> split(const std::string &txt, char delim);
 
 
 /**
- * Check if the given string ends with the ending.
+ * Check if a string ends with another given string.
+ *
+ * @param txt String that is checked.
+ * @param end String that is compared with the end of \p txt.
+ *
+ * @return true if \p txt ends with \p end, else false.
  */
 bool ends_with(const std::string &txt, const std::string &end);
 
 
 /**
- * Extend a vector with elements, without destroying source one.
+ * Extend a vector with elements, without destroying the
+ * source of elements.
+ *
+ * @tparam T Element type.
+ *
+ * @param vec Vector that is extended.
+ * @param ext Vector used as a source of elements.
  */
 template <typename T>
 void vector_extend(std::vector<T> &vec, const std::vector<T> &ext) {
-	vec.reserve(vec.size() + ext.size());
-	vec.insert(std::end(vec), std::begin(ext), std::end(ext));
+    vec.reserve(vec.size() + ext.size());
+    vec.insert(std::end(vec), std::begin(ext), std::end(ext));
 }
 
 
 /**
- * Extend a vector with elements with move semantics.
+ * Extend a vector with elements using move semantics.
+ *
+ * @tparam T Element type.
+ *
+ * @param vec Vector that is extended.
+ * @param ext Vector used as a source of elements.
  */
 template <typename T>
 void vector_extend(std::vector<T> &vec, std::vector<T> &&ext) {
-	if (vec.empty()) {
-		vec = std::move(ext);
-	}
-	else {
-		vec.reserve(vec.size() + ext.size());
-		std::move(std::begin(ext), std::end(ext), std::back_inserter(vec));
-		ext.clear();
-	}
+    if (vec.empty()) {
+        vec = std::move(ext);
+    }
+    else {
+        vec.reserve(vec.size() + ext.size());
+        std::move(std::begin(ext), std::end(ext), std::back_inserter(vec));
+        ext.clear();
+    }
 }
 
 
 /**
  * Membership check for some container.
+ *
+ * @tparam T Container type.
+ * @tparam V Type of checked value.
+ *
+ * @param container Container that is searched.
+ * @param value Value that is searched for.
+ *
+ * @return true if the value is in the container, else false.
  */
 template <typename T, typename V>
 bool contains(const T &container, const V &value) {
-	if (std::find(std::begin(container),
-	              std::end(container),
-	              value) == std::end(container)) {
-		return false;
-	}
-	else {
-		return true;
-	}
+    if (std::find(std::begin(container),
+                  std::end(container),
+                  value) == std::end(container)) {
+        return false;
+    }
+    else {
+        return true;
+    }
 }
 
 
 /**
  * Creates a hash value as a combination of two other hashes. Can be called incrementally to create
  * hash value from several variables.
+ *
+ * @param hash1 First hash.
+ * @param hash2 Second hash.
  */
 size_t hash_combine(size_t hash1, size_t hash2);
 
